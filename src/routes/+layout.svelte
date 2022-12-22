13a292ed47100e05700c06c0b4e29ed25e8458ff
marineve17 <mariana.lobao2002@gmail.com>
2022-12-22 20:04:51 +0000

    feat: background and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
   import '../app.css';
+  import Navbar from '$lib/layout/Navbar.svelte';
 </script>
 
-<slot />
+<style>
+  :global(html) {
+    min-height: 100vh;
+  }
+
+  :global(body) {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+<Navbar/>
+<div class="flex-grow bg-ni-primary">
+  <slot />
+</div>
