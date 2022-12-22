94b55e5c613891a252b2e0ec7c39f020751ed40b
toni-santos <a.oliveirasantos122@gmail.com>
2022-12-22 21:32:16 +0000

    Added first footer instance

---
+++ src/lib/layout/Footer.svelte
@@ -0,0 +1,41 @@
+<div class="w-full bg-secondary p-3 max-sm:hidden">
+    <footer class="p-2 text-white flex flex-row justify-between border-t-2">
+        <div class="flex p-3 self-center">
+            ICONS
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="/images/ni_negative_logo.svg" class="w-11 h-auto">
+            <p class="text-sm">&copy; NIAEFEUP 2022</p>
+        </div>
+        <div class="flex flex-row self-center items-center gap-2">
+            <div class="text-sm text-right">
+                <p class="font-bold">FEUP</p>
+                <p>Sala B315</p>
+            </div>    
+            <a>ICON</a>
+        </div>
+    </footer>
+</div>
+
+<div class="hidden w-full bg-secondary p-3 max-sm:block">
+    <footer class="p-2 text-white flex flex-col justify-between border-t-2 text-sm">
+        <div class="flex flex-row p-3 self-center justify-between w-full items-center">
+            <span>NIAEFEUP</span>
+            <img src="/images/ni_negative_logo.svg" class="w-11 h-auto">
+            <div class="flex flex-row gap-1">
+                <span class="font-bold">FEUP</span>
+                <span>B315</span>
+            </div>
+        </div>
+        <div class="flex flex-col items-center p-2">
+            ICONS
+        </div>
+        <div class="flex flex-row self-center items-center gap-2">
+            <div class="bg-red-500 w-16 flex flex-row justify-between rounded-md content-center">
+                <div class="bg-red-400 w-5 rounded-l-md"></div>
+                <span class="pr-1">login</span>
+            </div>
+        </div>
+        <span class="self-center pt-4">&copy; NIAEFEUP 2022</span>
+    </footer>
+</div>
