add65197abe63a76a0e7b7fe4958c65ecdd44226
marineve17 <mariana.lobao2002@gmail.com>
2022-12-22 20:04:51 +0000

    feat: background and navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const primaryColor = "#872020";
+const secondaryColor = "#B33636";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "primary": primaryColor,
+        "secondary": secondaryColor,
+      },
+
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif']
+      },
+    },
   },
   plugins: []
 };
