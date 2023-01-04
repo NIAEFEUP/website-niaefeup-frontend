2d0f2df315bef70863368801140f771a91f0f81b
Bruno Mendes <bd_mendes@outlook.com>
2023-01-04 12:01:24 +0000

    Add test pages

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { fetchWithAuth } from '$lib/network';
+  import { onMount } from 'svelte';
+
+  let info = 'Loading...';
+
+  onMount(async () => {
+    const response = await fetchWithAuth('/auth');
+    const json = await response.json();
+    info = JSON.stringify(json);
+  });
+</script>
+
+<div>
+  <h1>Profile</h1>
+  <p>{info}</p>
+</div>
