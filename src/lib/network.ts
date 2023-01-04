7ce7497c6cab8cddef3e93a9e3a7469f37d1ca85
Bruno Mendes <bd_mendes@outlook.com>
2023-01-04 12:01:24 +0000

    Add test pages

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -3,8 +3,8 @@ import { PUBLIC_API_URL } from '$env/static/public'
 const JWT_ACCESS_STORAGE_KEY = 'jwt'
 const JWT_REFRESH_STORAGE_KEY = 'jwt-refresh'
 
-async function refreshAccessToken(): Promise<boolean> {
-    const jwtRefresh = localStorage.getItem('jwt-refresh')
+async function _refreshAccessToken(): Promise<boolean> {
+    const jwtRefresh = localStorage.getItem(JWT_REFRESH_STORAGE_KEY)
     if (!jwtRefresh) {
         return false
     }
@@ -22,12 +22,14 @@ async function refreshAccessToken(): Promise<boolean> {
     }
 
     const json = await response.json()
-    localStorage.setItem(JWT_ACCESS_STORAGE_KEY, json.accessToken)
+    localStorage.setItem(JWT_ACCESS_STORAGE_KEY, json['access_token'])
     return true
 }
 
-export async function fetchWithAuth(relativeUrl: URL, method: string, headers: Headers, body: Object): Promise<Response> {
-    const fetchAdapter = (relativeUrl: URL, method: string, headers: Headers, body: Object) => {
+export async function _fetchWithAuth(allowRefresh: boolean, relativeUrl: URL | string,
+    method: string, headers?: HeadersInit, body?: Object): Promise<Response> {
+
+    const fetchAdapter = (relativeUrl: URL | string, method: string, headers: Headers, body?: Object) => {
         const url = new URL(relativeUrl, PUBLIC_API_URL)
         headers.append('Content-Type', 'application/json')
         return fetch(url, { method: method, body: JSON.stringify(body), headers })
@@ -35,20 +37,25 @@ export async function fetchWithAuth(relativeUrl: URL, method: string, headers: H
 
     const jwt = localStorage.getItem(JWT_ACCESS_STORAGE_KEY)
     if (!jwt) {
-        return fetchAdapter(relativeUrl, method, headers, body)
+        return fetchAdapter(relativeUrl, method, new Headers(headers), body)
     }
 
     const authHeaders = new Headers(headers)
-    headers.append('Authorization', `Bearer ${jwt}`)
+    authHeaders.append('Authorization', `Bearer ${jwt}`)
 
     const response = await fetchAdapter(relativeUrl, method, authHeaders, body)
-    if (response.status === 401) {
-        const refreshedAccessTokenSuccessful = await refreshAccessToken()
+    if (response.status === 401 && allowRefresh) {
+        const refreshedAccessTokenSuccessful = await _refreshAccessToken()
         if (refreshedAccessTokenSuccessful) {
-            return fetchWithAuth(relativeUrl, method, headers, body)
+            return _fetchWithAuth(false, relativeUrl, method, headers, body)
         }
     }
     return response
+}
+
+export async function fetchWithAuth(relativeUrl: URL | string,
+    method: string = "GET", headers?: HeadersInit, body?: Object): Promise<Response> {
+    return _fetchWithAuth(true, relativeUrl, method, headers, body)
 }
 
 export async function login(email: string, password: string): Promise<boolean> {
@@ -65,8 +72,8 @@ export async function login(email: string, password: string): Promise<boolean> {
     }
 
     const json = await response.json()
-    localStorage.setItem(JWT_ACCESS_STORAGE_KEY, json.accessToken)
-    localStorage.setItem(JWT_REFRESH_STORAGE_KEY, json.refreshToken)
+    localStorage.setItem(JWT_ACCESS_STORAGE_KEY, json['access_token'])
+    localStorage.setItem(JWT_REFRESH_STORAGE_KEY, json['refresh_token'])
     return true
 }
 
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { login } from '$lib/network';
+  let email = '';
+  let password = '';
+  let message = '';
+
+  async function submitLogin() {
+    const success = await login(email, password);
+    message = success ? 'Login successful' : 'Login failed';
+  }
+</script>
+
+<form on:submit={submitLogin}>
+  <input type="text" name="email" placeholder="email" bind:value={email} />
+  <br />
+  <input type="password" name="password" placeholder="password" bind:value={password} />
+  <br />
+  <button type="submit">Login</button>
+  <br />
+  <p>{message}</p>
+</form>
