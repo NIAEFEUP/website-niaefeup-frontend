96341782616ecf1281e452d6fa424989b2cbc647
André Lima <me@limwa.pt>
2023-01-13 17:39:37 +0000

    feat: initial work on hexagons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import HexagonImage from '@/components/Hexagons/HexagonImage.svelte';
+</script>
+
+<div class="w-1/4 min-h-screen px-20 py-10 bg-gray-800">
+    <HexagonImage src="/members/bruno-rosendo.png" alt="Bruno Rosendo's profile" bg="bg-gray-800" />
+</div>
+    <!-- <div> -->
+        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatem a aliquid ea adipisci natus ab aspernatur quos in tenetur commodi laudantium labore velit neque ducimus, delectus sunt. Aut, odio.</p> -->
+        <!-- <strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ex itaque quos minus qui autem! Natus consectetur laudantium sapiente delectus eligendi fuga voluptatibus minima voluptatem ratione temporibus esse, perferendis possimus?</strong> -->
+    <!-- </div> -->
