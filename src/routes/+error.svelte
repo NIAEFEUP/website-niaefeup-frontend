6e664f8ad6e9a3176814a81398d7c34eef169f56
pedrosilva17 <pedroafsuper@gmail.com>
2023-01-20 19:53:50 +0000

    Changed div to main tag, added simple button functionality

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
   import { page } from '$app/stores';
 </script>
 
-<div class="flex flex-col font-source-code-pro justify-center my-20 px-10 gap-20">
+<main class="flex flex-col font-source-code-pro justify-center my-20 mx-10 gap-20">
     <section class="flex flex-col flex-1 text-white text-center break-normal gap-10">
         <h1 class="text-7xl md:text-huge font-extrabold">{$page.status}</h1>
         <h2 class="text-3xl md:text-6xl font-semibold">missing ; before statement</h2>
@@ -17,7 +17,7 @@
             <h2 class="text-xl md:text-4xl font-medium">There is a semicolon (;) missing somewhere</h2>
         </section>
     </div>
-    <button class="flex bg-primary hover:bg-secondary m-auto text-white font-raleway font-bold min-w-50 py-2 px-4 rounded">
+    <a href="/" class="flex bg-primary hover:bg-secondary m-auto text-white font-raleway font-bold min-w-50 py-2 px-4 rounded">
         <p class="px-5">Voltar</p>
-    </button>
-</div>
+    </a>
+</main>
