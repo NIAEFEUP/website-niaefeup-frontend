0990a65ec67ba787188d5f41257cca5a5eb3766c
marineve17 <mariana.lobao2002@gmail.com>
2023-01-25 16:12:06 +0000

    improve sidebar logic

--- src/lib/layout/Sidebar.svelte
+++ src/lib/layout/Sidebar.svelte
@@ -1,26 +1,26 @@
+<script lang="ts">
+    import NavItem from "$lib/navbar/NavItem.svelte";
+
+    let selectedIndex: number | null = null;
+
+    const items = [
+        "Início",
+        "Eventos",
+        "Projetos",
+        "Equipa",
+        "Contactos",
+    ];
+</script>
+
 <nav class="bg-ni-sidebar grid grid-cols-4 grid-rows-6 justify-items-center sm:invisible py-4 absolute w-screen h-screen">
     <div class="col-start-4 flex items-start justify-end">
         <button class="text-white text-4xl">X</button>
     </div>
     <ul class="row-start-2 col-start-1 col-span-3 pl-4 text-white text-4xl font-source-code flex flex-col gap-12">
-        <li>
-            <a href="">Início</a>
-        </li>
-
-        <li>
-            <a href="">Eventos</a>
-        </li>
-
-        <li>
-            <a href="">Projetos</a>
-        </li>
-
-        <li>
-            <a href="">Equipa</a>
-        </li>
-
-        <li>
-            <a href="">Contactos</a>
-        </li>
+        {#each items as item, i}
+            <NavItem selected={selectedIndex === i} onSelect={() => {selectedIndex = i}}>
+                <a href="">{item}</a>
+            </NavItem>
+        {/each}
     </ul>
 </nav>
