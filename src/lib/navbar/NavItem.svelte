3539e6f9a396982a0258fd806c563a77507aaaf1
marineve17 <mariana.lobao2002@gmail.com>
2023-01-25 16:12:06 +0000

    improve sidebar logic

---
+++ src/lib/navbar/NavItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let selected: boolean;
+    export let onSelect: () => void;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<li on:click={onSelect}>
+    {#if selected}
+        <strong>
+            <p>&lt;</p>
+            <slot />
+            <p>&gt;</p>
+        </strong>
+    {:else}
+        <slot />
+    {/if}
+</li>
