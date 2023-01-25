b11602f126b9d398a7f47c3668494ee458f6e6ba
toni-santos <a.oliveirasantos122@gmail.com>
2023-01-25 18:07:16 +0000

    layout adjustments to better fit the intended design

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -1,10 +1,12 @@
-<nav class="bg-secondary text-white collapse sm:visible font-raleway flex justify-between items-center h-min px-7 py-1">
+<nav class="fixed bg-secondary shadow-lg text-white text-xs sm:text-base font-raleway w-full grid grid-cols-3 justify-center items-center h-min px-7 py-1">
     <div class="flex gap-7">
         <a href="">Equipa</a>
         <a href="">Projetos</a>
     </div>
-    <img src="/images/ni_logo.png" alt="NIAEFEUP's logo" width="40" height="40">
-    <div class="flex gap-7">
+    <div class="flex w-full justify-center">
+        <img src="/images/ni_logo.png" alt="NIAEFEUP's logo" width="40" height="40">
+    </div>
+    <div class="flex gap-7 justify-end">
         <a href="">Eventos</a>
         <a href="">Contactos</a>
     </div>
