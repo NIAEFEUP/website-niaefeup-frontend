1b8a97ab8bf80ebba749bff44de580e0f91ca518
André Lima <me@limwa.pt>
2023-02-05 02:47:50 +0000

    feat: finish single hexagon

---
+++ src/components/Hexagons/Hexagon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let className: string = "";
+</script>
+
+<div class="clip-background bg-white {className}">
+  <div class="clip-foreground flex items-center justify-center">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .clip-background, .clip-foreground {
+    aspect-ratio: 316 / 270; 
+    mask-size: cover;
+  }
+
+  .clip-background {
+    mask-image: url("/masks/hexagon/background.svg");
+  }
+
+  .clip-foreground {
+    mask-image: url("/masks/hexagon/foreground.svg");
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
-    import HexagonImage from '@/components/Hexagons/HexagonImage.svelte';
+    import Hexagon from '@/components/Hexagons/Hexagon.svelte';
 </script>
 
 <div class="w-1/4 min-h-screen px-20 py-10 bg-gray-800">
-    <HexagonImage src="/members/bruno-rosendo.png" alt="Bruno Rosendo's profile" bg="bg-gray-800" />
+    <Hexagon>
+        <p>testing the hexagon clipping. does this text clip?</p>
+    </Hexagon>
 </div>
-    <!-- <div> -->
-        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatem a aliquid ea adipisci natus ab aspernatur quos in tenetur commodi laudantium labore velit neque ducimus, delectus sunt. Aut, odio.</p> -->
-        <!-- <strong>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ex itaque quos minus qui autem! Natus consectetur laudantium sapiente delectus eligendi fuga voluptatibus minima voluptatem ratione temporibus esse, perferendis possimus?</strong> -->
-    <!-- </div> -->
