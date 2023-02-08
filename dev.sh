ec8c7376958303e6128f8fae8f517dfb5518a69e
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-02-08 15:22:00 +0000

    Added script for tests and dev environment

---
+++ dev.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+which docker-compose &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Docker compose not installed"
+    echo "Please read the README at https://github.com/NIAEFEUP/website-niaefeup-frontend"
+    exit
+fi
+
+docker-compose up website-niaefeup-frontend-dev -d
+docker-compose up website-niaefeup-frontend-storybook -d
+
+trap_ctrl_c() {
+    docker-compose down
+    clear
+
+    exit
+}
+
+trap "trap_ctrl_c" 2
+
+clear
+echo "Containers are up and running"
+echo ""
+echo "Press Ctrl-C to exit"
+
+while true; do
+    sleep 1
+done
