5561d5db3d9189b7ecab60ff49de8dc39b32ce71
Bruno Mendes <bd_mendes@outlook.com>
2023-02-23 14:43:45 +0000

    Simplify fetch adapter

--- src/routes/api/[...endpoint]/proxy.ts
+++ src/routes/api/[...endpoint]/proxy.ts
@@ -2,15 +2,25 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import type { Cookies } from '@sveltejs/kit';
 import { JWT_REFRESH_KEY, JWT_ACCESS_KEY } from '$lib/auth';
 
+async function _fetchAdapter(
+  relativeUrl: URL | string,
+  method: string,
+  body?: string | undefined,
+  headers?: Headers | undefined
+) {
+  const url = new URL(relativeUrl, PUBLIC_API_URL);
+  headers ??= new Headers();
+  headers.append('Content-Type', 'application/json');
+  return fetch(url, { method: method, body, headers });
+}
+
 async function _refreshAccessToken(cookies: Cookies): Promise<boolean> {
   const refreshToken = cookies.get(JWT_REFRESH_KEY);
-  const response = await fetch(`${PUBLIC_API_URL}/auth/refresh`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ token: refreshToken })
-  });
+  const response = await _fetchAdapter(
+    'auth/refresh',
+    'POST',
+    JSON.stringify({ token: refreshToken })
+  );
   return response.ok;
 }
 
@@ -22,26 +32,15 @@ async function _fetchWithAuth(
   headers?: HeadersInit,
   body?: string | undefined
 ): Promise<Response> {
-  const fetchAdapter = (
-    relativeUrl: URL | string,
-    method: string,
-    headers: Headers,
-    body?: string | undefined
-  ) => {
-    const url = new URL(relativeUrl, PUBLIC_API_URL);
-    headers.append('Content-Type', 'application/json');
-    return fetch(url, { method: method, body, headers });
-  };
-
   const jwt = cookies.get(JWT_ACCESS_KEY);
   if (!jwt) {
-    return fetchAdapter(relativeUrl, method, new Headers(headers), body);
+    return _fetchAdapter(relativeUrl, method, body, new Headers(headers));
   }
 
   const authHeaders = new Headers(headers);
   authHeaders.append('Authorization', `Bearer ${jwt}`);
 
-  const response = await fetchAdapter(relativeUrl, method, authHeaders, body);
+  const response = await _fetchAdapter(relativeUrl, method, body, authHeaders);
   if (response.status >= 400 && response.status < 500 && allowRefresh) {
     const refreshedAccessTokenSuccessful = await _refreshAccessToken(cookies);
     if (refreshedAccessTokenSuccessful) {
