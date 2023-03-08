4aaaf0347c7548cf0d2e8f0d2e94452393cfc8a2
marineve17 <mariana.lobao2002@gmail.com>
2023-03-08 16:54:41 +0000

    implement new navbar design

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -1,16 +1,15 @@
 <nav
-  class="fixed hidden h-min w-full grid-cols-3 items-center justify-center bg-secondary px-7 py-1 font-raleway text-xs text-white shadow-lg sm:grid sm:text-base"
+  class="fixed hidden h-min w-full grid-cols-2 items-center justify-center px-7 py-1 font-raleway text-xs text-white sm:grid sm:text-base"
 >
-  <div class="flex gap-7">
-    <a href="#/">Equipa</a>
-    <a href="#/">Projetos</a>
-  </div>
-  <div class="flex w-full justify-center">
-    <a href="#/">
+  <div class="flex w-full justify-start">
+    <a href="#/" class="flex items-center gap-4">
       <img src="/images/ni_logo.png" alt="NIAEFEUP's logo" width="40" height="40" />
+      <span>NIAEFEUP</span>
     </a>
   </div>
   <div class="flex justify-end gap-7">
+    <a href="#/">Equipa</a>
+    <a href="#/">Projetos</a>
     <a href="#/">Eventos</a>
     <a href="#/">Contactos</a>
   </div>
