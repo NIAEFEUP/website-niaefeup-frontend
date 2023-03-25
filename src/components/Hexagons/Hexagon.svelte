987510ac16ca7ec62fd637832a31c52c26e27100
André Lima <me@limwa.pt>
2023-03-25 11:46:23 +0000

    chore: format files

--- src/components/Hexagons/Hexagon.svelte
+++ src/components/Hexagons/Hexagon.svelte
@@ -1,27 +1,28 @@
 <div class="clip-ring bg-white">
   <div class="clip-outer-hexagon flex items-center justify-center">
-    <div class="clip-hexagon w-full h-full">
-        <slot />
+    <div class="clip-hexagon h-full w-full">
+      <slot />
     </div>
   </div>
 </div>
 
 <style>
-  .clip-ring, .clip-outer-hexagon {
+  .clip-ring,
+  .clip-outer-hexagon {
     aspect-ratio: 316 / 270; /* from Figma */
     mask-size: cover;
   }
-  
+
   .clip-ring {
     clip-path: polygon(0% 50%, 25% 100%, 75% 100%, 100% 50%, 75% 0%, 25% 0%);
-    mask-image: url("/masks/hexagon/background.svg");
+    mask-image: url('/masks/hexagon/background.svg');
   }
-  
+
   .clip-outer-hexagon {
     padding: 5.8% 6.9%; /* Magic Numbers */
-    mask-image: url("/masks/hexagon/foreground.svg");
+    mask-image: url('/masks/hexagon/foreground.svg');
   }
-  
+
   .clip-hexagon {
     clip-path: polygon(0% 50%, 25% 100%, 75% 100%, 100% 50%, 75% 0%, 25% 0%);
   }
--- src/components/Hexagons/HexagonGrid.svelte
+++ src/components/Hexagons/HexagonGrid.svelte
@@ -1,37 +1,45 @@
 <script lang="ts">
-    // Inspired by https://github.com/sveltejs/svelte-virtual-list/blob/master/VirtualList.svelte
-    type T = $$Generic;
+  // Inspired by https://github.com/sveltejs/svelte-virtual-list/blob/master/VirtualList.svelte
+  type T = $$Generic;
 
-    export let items: T[];
-    export let cols: number;
+  export let items: T[];
+  export let cols: number;
 
-    export let gap: "small" | "medium" | "big" = "medium";
+  export let gap: 'small' | 'medium' | 'big' = 'medium';
 </script>
 
 <!-- Warning: Very magic, totally eyeballed numbers in the next line -->
-<ul class="grid gap-{gap}" style="container-name: hgrid; container-type: inline-size; grid-template-columns: 0.255fr repeat({cols}, 0.492fr 0.255fr); grid-auto-rows: 0.4991fr 0.5fr;"> 
-    {#each items as item, index}
-        {@const col = index % cols}
-        {@const row = (index - col) / cols}
-        <li style="grid-column: {2 * col + 1} / {2 * col + 4}; grid-row: {2 * row + 1 + col % 2} / {2 * row + 3 + col % 2}">
-            <slot {item} {col} {row} />
-        </li>
-    {/each}
+<ul
+  class="grid gap-{gap}"
+  style="container-name: hgrid; container-type: inline-size; grid-template-columns: 0.255fr repeat({cols}, 0.492fr 0.255fr); grid-auto-rows: 0.4991fr 0.5fr;"
+>
+  {#each items as item, index}
+    {@const col = index % cols}
+    {@const row = (index - col) / cols}
+    <li
+      style="grid-column: {2 * col + 1} / {2 * col + 4}; grid-row: {2 * row + 1 + (col % 2)} / {2 *
+        row +
+        3 +
+        (col % 2)}"
+    >
+      <slot {item} {col} {row} />
+    </li>
+  {/each}
 </ul>
 
 <style>
-    .gap-small {
-        column-gap: 1%;
-        row-gap: 3%;
-    }
+  .gap-small {
+    column-gap: 1%;
+    row-gap: 3%;
+  }
 
-    .gap-medium {
-        column-gap: 2%;
-        row-gap: 5.5%;
-    }
+  .gap-medium {
+    column-gap: 2%;
+    row-gap: 5.5%;
+  }
 
-    .gap-big {
-        column-gap: 3%;
-        row-gap: 9%;
-    }
+  .gap-big {
+    column-gap: 3%;
+    row-gap: 9%;
+  }
 </style>
