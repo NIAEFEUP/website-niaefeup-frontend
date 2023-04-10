f6157aeafd2b190597ee6ebf3562022a07f30e3a
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-04-10 01:18:16 +0100

    Created stories for snackbars

---
+++ src/lib/notifications/Snackbar.stories.ts
@@ -0,0 +1,25 @@
+import { Notification } from ".";
+import Snackbar from "./Snackbar.svelte";
+
+export default {
+  title: 'Snackbar',
+  component: Snackbar,
+  argTypes: {
+    notification: { control: 'object' },
+  },
+  parameters: {
+    layout: 'centered'
+  }
+};
+
+export const ShortNotification = {
+  args: {
+    notification: new Notification('This is a short notification')
+  }
+};
+
+export const LongNotification = {
+  args: {
+    notification: new Notification('This is a long notification that wraps in multiple lines. Do you like it? ;)')
+  }
+};
