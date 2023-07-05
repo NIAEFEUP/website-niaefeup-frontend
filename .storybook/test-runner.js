87cb60910b32fc4e90a8e9aca5a74020f68a687b
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-07-05 18:51:15 +0100

    Added a11y to test runner

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,5 +1,6 @@
 const { getStoryContext } = require('@storybook/test-runner');
 const { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } = require('@storybook/addon-viewport');
+const { injectAxe, checkA11y, configureAxe } = require('axe-playwright');
 
 // Set the viewport to the default viewport for the story
 module.exports = {
@@ -21,5 +22,28 @@ module.exports = {
       );
 
     if (parsedViewportSizes) page.setViewportSize(parsedViewportSizes);
+
+    await injectAxe(page); // For a11y testing
+  },
+
+  async postRender(page, story) {
+    const storyContext = await getStoryContext(page, story);
+
+    // Do not run a11y tests on disabled stories.
+    if (storyContext.parameters?.a11y?.disable) {
+      return;
+    }
+
+    // Apply story-level a11y rules
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules
+    });
+
+    await checkA11y(page, '#storybook-root', {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true
+      }
+    });
   }
 };
