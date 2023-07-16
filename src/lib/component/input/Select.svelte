54714c1f99d558ebcaf41fd022ce3037ed95af57
André Moreira <andre.moreira.9@hotmail.com>
2023-07-16 10:35:41 +0100

    Add headlessui package

--- src/lib/component/input/Select.svelte
+++ src/lib/component/input/Select.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
-    import Select2 from 'svelte-select/no-styles/Select.svelte';
+  import {
+    Listbox,
+    ListboxButton,
+    ListboxOption,
+    ListboxOptions
+  } from '@rgossiaux/svelte-headlessui';
+  import type { SupportedAs } from '@rgossiaux/svelte-headlessui/internal/elements';
+  import { onMount } from 'svelte';
 
-    const items = [
-        {value: 1, label: "Ola"},
-        {value: 2, label: "Deus"},
-        {value: 3, label: "Siuuuyuuuuu"},
-    ]
+  const items = [
+    { value: 1, label: 'Ola' },
+    { value: 2, label: 'Deus' },
+    { value: 3, label: 'Siuuuyuuuuu' }
+  ];
+
+  let selectedItem: { label: any } | null = null;
+
+  let _class = '';
+  export { _class as class };
 </script>
 
-
-<Select2 placeholder="Filtros" items={items} searchable={false} />
+<Listbox bind:value={selectedItem} class={_class}>
+  <ListboxButton
+    class="relative flex h-10 w-full rounded-lg border-0 bg-stone-400/25 px-5 text-xl text-white outline-none aria-expanded:rounded-none aria-expanded:rounded-t-lg"
+  >
+    <div class={`flex h-full flex-col justify-center ${selectedItem ? '' : 'text-gray-400'}`}>
+      {selectedItem?.label || 'Placeholder'}
+    </div>
+  </ListboxButton>
+  <ListboxOptions class="bottom-full w-full rounded-b-lg bg-stone-400/25 last:rounded-b-lg">
+    {#each items as item (item.value)}
+      <ListboxOption class="cursor-pointer text-xl text-white hover:bg-stone-200/25" value={item}>
+        {item.label}
+      </ListboxOption>
+    {/each}
+  </ListboxOptions>
+</Listbox>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-    import Icons from "$lib/component/Icons";
-    import Select from "$lib/component/input/Select.svelte";
-  import TextField from "$lib/component/input/TextField.svelte";
-  import Title from "$lib/component/Title.svelte";
+  import Icons from '$lib/component/Icons';
+  import Select from '$lib/component/input/Select.svelte';
+  import TextField from '$lib/component/input/TextField.svelte';
+  import Title from '$lib/component/Title.svelte';
 
   const searchEvents = (e: SubmitEvent) => {
-    console.log(new FormData(e.target as HTMLFormElement))
-  }
-
+    console.log(new FormData(e.target as HTMLFormElement));
+  };
 </script>
 
 <div class="my-20">
-    <div class="flex justify-center">
-        <Title title="Eventos" />
-    </div>
+  <div class="flex justify-center">
+    <Title title="Eventos" />
+  </div>
 
-    <form class="flex flex-wrap justify-center mt-10" on:submit={searchEvents}>
-        <TextField
-            class="mx-5"
-            beforeIcon={{button: true, icon: Icons.Search, type: "submit"}}
-            name="search"
-            placeholder="Search"
-            value=""
-        />
-        <Select></Select>
-    </form>
+  <form class="mt-10 flex flex-wrap justify-center" on:submit={searchEvents}>
+    <TextField
+      class="mx-5"
+      beforeIcon={{ button: true, icon: Icons.Search, type: 'submit' }}
+      name="search"
+      placeholder="Search"
+      value=""
+    />
+
+    <Select />
+  </form>
 </div>
