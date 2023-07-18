e73b609eec14ae732a1b9d1518fa8cae61ffba73
Rita Lopes <up202108819@up.pt>
2023-07-18 15:30:25 +0100

    Renamed and added custom colors

--- src/lib/notifications/Snackbar.svelte
+++ src/lib/notifications/Snackbar.svelte
@@ -10,7 +10,7 @@
 <div
   out:slide
   in:fly={{ x: 100 }}
-  class="mb-4 flex h-auto w-80 rounded-lg bg-primary px-3 py-2"
+  class="mb-4 flex h-auto w-80 rounded-lg bg-muted-red-700 px-3 py-2"
   role="status"
 >
   <div class="m-auto ml-3 font-medium text-white">
@@ -18,7 +18,7 @@
   </div>
   <div class="ml-1 flex flex-col">
     <button
-      class="rounded-lg p-1.5 hover:bg-secondary"
+      class="rounded-lg p-1.5 hover:bg-muted-red-500"
       on:click={() => notification.close()}
       aria-label="Close"
     >
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,34 @@
-const primaryColor = '#872020FF';
-const secondaryColor = '#B33636FF';
-const tertiaryColor = '#C04343FF';
-const tertiary60Color = '#C0434399';
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts,stories.js,stories.ts}'],
   theme: {
     extend: {
       colors: {
-        primary: primaryColor,
-        secondary: secondaryColor,
-        tertiary: tertiaryColor,
-        tertiary60: tertiary60Color
+        taupe: {
+          300: '#AB8586',
+          200: '#D7C0C0',
+          100: '#FFE9E9'
+        },
+        'vivid-red': {
+          950: '#4D0000',
+          900: '#570000',
+          700: '#A40202',
+          400: '#DC4F47'
+        },
+        'muted-red': {
+          800: '#740F0F',
+          700: '#872020',
+          600: '#9D2B2B',
+          500: '#B33636',
+          400: '#C04343',
+          300: '#DF6363'
+        },
+        rose: {
+          950: '#411315',
+          600: '#8D3739',
+          400: '#A46868',
+          200: '#DAB5B5'
+        }
       },
 
       fontFamily: {
