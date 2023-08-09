97078054982ef567ceb7214bf9a5ed0de76e3031
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-08-09 16:06:51 +0100

    HTML first prototype

---
+++ src/lib/layout/HorizontalTimeline.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-full grid grid-cols-1 grid-rows-1">
+    <!-- Bar -->
+    <div class="w-9/12 h-1.5 bg-taupe-300 rounded-lg m-auto z-1 row-span-full col-span-full"></div>
+    <!-- Points -->
+    <div class="w-9/12 flex flex-row m-auto z-2 row-span-full col-span-full justify-around">
+        <div class="flex flex-col justify-start items-center mb-6">
+            <p class="text-taupe-100">19/20</p>
+            <div class="w-4 h-4 rounded-full bg-taupe-100">
+            </div>
+        </div>
+        <div class="flex flex-col justify-start items-center mb-6">
+            <p class="text-taupe-300">20/21</p>
+            <div class="w-4 h-4 rounded-full bg-taupe-300">
+            </div>
+        </div>
+        <div class="flex flex-col justify-start items-center mb-6">
+            <p class="text-taupe-300">21/22</p>
+            <div class="w-4 h-4 rounded-full bg-taupe-300">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import HorizontalTimeline from "@/lib/layout/HorizontalTimeline.svelte";
+
+</script>
+<section class="absolute mx-auto flex flex-col items-center h-screen w-full min-w-[40%] justify-center gap-10">
+    <h1 class="text-white font-source_code text-3xl font-medium">&lt; <span class="font-bold">Equipa</span> /&gt;</h1>
+    <HorizontalTimeline></HorizontalTimeline>
+</section>
