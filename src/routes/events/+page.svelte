cfaa235c0f2fc0a02fb042b413780df3609fc3db
André Moreira <andre.moreira.9@hotmail.com>
2023-08-11 21:43:19 +0100

    Fix navbar not clickable and add events title

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Title from "$lib/component/Title.svelte";
+
+  export let data: { info: string };
+  let logoutMessage = '';
+
+  async function logout() {
+    const response = await fetch('/api/auth/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    logoutMessage = response.ok ? 'Logout successful' : 'Logout failed';
+  }
+</script>
+
+<div class="my-20">
+    <div class="flex justify-center">
+        <Title title="Eventos" />
+    </div>
+</div>
