b9fae4bde4e3604cd38fb94f2c1f1c8d629bc373
jacribeiro <j.alberto.cribeiro@gmail.com>
2024-02-08 10:10:01 +0000

    Completed design for project listing page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,7 +11,8 @@
 <div class='flex flex-col items-center'>
     <h1 class='font-source_code text-white text-xl my-10 md:text-3xl md:my-20'>&lt&nbsp<strong>Projetos</strong>&nbsp/&gt</h1>
 
-    <div class="w-full h-full flex flex-col flex-wrap justify-evenly gap-4 md:flex-row md:items-end md:gap-x-1.5">
+    <div class="w-full h-full flex flex-col flex-wrap justify-evenly gap-4 -mt-1
+                md:flex-row md:items-end md:gap-x-1.5 md:mt-0">
         {#each projects as project}
             <ProjectTile name={project.name} />
         {/each}
--- src/routes/projects/ProjectTile.svelte
+++ src/routes/projects/ProjectTile.svelte
@@ -4,15 +4,30 @@
     export let
         name: string;
     const colorList = {
-        nijobs: 'bg-[#DC4F47]',
-        uni: 'bg-[#75171E]',
-        tts: 'bg-[#E7E7E7]'
+        nijobs: {
+            background: 'bg-[#9c342f]',
+            text: 'text-gray-200'
+        },
+        uni: {
+            background:'bg-[#521015]',
+            text: 'text-gray-200'
+        },
+        tts: {
+            background: 'bg-[#A8A8A8]',
+            text: 'text-gray-900'
+        }
     };
 </script>
 
 <svelte:window bind:innerWidth={windowWidth}></svelte:window>
 
-<div class='bg-muted-red-500 h-40 rounded-xl flex flex-col justify-end w-4/5 py-3 odd:self-start even:self-end odd:items-end md:self-center md:w-auto md:h-auto md:py-0 md:flex-none md:bg-transparent md:basis-3/10 md:justify-between md:items-center md:gap-4'>
-    <img class='h-20 md:h-72' src={'/images/'+ name.toLowerCase() + (windowWidth >= 768 ? '.png' : '_logo.png')} alt='nijobs logo'/>
-    <p class='font-raleway font-semibold text-gray-200 text-2xl md:text-white md:text-5xl'>{name}</p>
+<div class='{colorList[name.toLowerCase()].background} h-44 w-4/5 rounded-xl flex flex-col justify-center
+    {windowWidth < 768 ? 'odd:self-start even:self-end odd:items-end even:items-start' : null}
+    md:self-center md:w-auto md:h-auto md:flex-none md:bg-transparent '>
+    <div class='flex flex-col items-center py-3 px-4 -mb-3 mx-5 gap-1
+                md:justify-between md:px-0 md:py-0 md:gap-4 md:basis-3/10'>
+        <img class='h-20 shadow-lg rounded-xl md:shadow-none md:h-72' src={'/images/'+ name.toLowerCase() + (windowWidth >= 768 ? '.png' : '_logo.png')} alt='nijobs logo'/>
+        <p class='font-raleway font-semibold text-2xl {colorList[name.toLowerCase()].text}
+                  md:text-white md:text-5xl'>{name}</p>
+    </div>
 </div>
