ee36d0c720a0a559b17ed2defc3ab67abde84aac
Rita Lopes <up202108819@up.pt>
2024-04-25 13:33:07 +0100

    Restructured project files

---
+++ src/routes/(app)/_components/layout/notifications/snackbar-list.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Snackbar from './snackbar.svelte';
+  import { notifications } from './index';
+</script>
+
+<div class="fixed bottom-4 right-10 z-20">
+  {#each $notifications as notification (notification.id)}
+    <Snackbar {notification} />
+  {/each}
+</div>
