516dd43982a71b9998ab54aabb1a690c16c6bab1
jpnsantoss <jpnsantos14@gmail.com>
2024-06-13 16:19:05 +0100

    responsivity

--- src/routes/(app)/events/[slug]/_components/event-header.svelte
+++ src/routes/(app)/events/[slug]/_components/event-header.svelte
@@ -13,37 +13,44 @@
   let time = format(date, "HH'h'mm");
 </script>
 
-<div class="grid gap-12 md:grid-cols-3">
-  <div class="space-y-12 text-white md:col-span-2">
-    <h1 class="text-6xl font-bold">{data.title}</h1>
-    <ul class="space-y-4">
-      <li class="text-xl capitalize">{weekday} - {time}</li>
-      <li class="flex items-center gap-2 text-xl font-bold">
-        <Icon
-          src={Icons.Calendar}
-          color="white"
-          size="20px"
-          href="https://www.instagram.com/niaefeup/"
-          ariaLabel="Instagram"
-        />
-        {format(date, 'd MMMM yyyy', { locale: pt })}
-      </li>
-      <li class="flex w-full items-center gap-2 text-xl">
-        <Icon
-          src={Icons.Location}
-          color="white"
-          size="20px"
-          href="https://www.instagram.com/niaefeup/"
-          ariaLabel="Instagram"
-        />
-        {data.location}
-      </li>
-    </ul>
+<div class="flex flex-col items-center gap-12">
+  <div class="grid grid-cols-2 gap-12 md:grid-cols-3">
+    <div class="space-y-12 text-white md:col-span-2">
+      <h1 class="hidden text-6xl font-bold md:block">{data.title}</h1>
+      <ul class="space-y-4">
+        <li class="text-xl capitalize">{weekday} - {time}</li>
+        <li class="flex items-center gap-2 text-xl font-bold">
+          <Icon
+            src={Icons.Calendar}
+            color="white"
+            size="20px"
+            href="https://www.instagram.com/niaefeup/"
+            ariaLabel="Instagram"
+          />
+          {format(date, 'd MMMM yyyy', { locale: pt })}
+        </li>
+        <li class="flex w-full items-center gap-2 text-xl">
+          <Icon
+            src={Icons.Location}
+            color="white"
+            size="20px"
+            href="https://www.instagram.com/niaefeup/"
+            ariaLabel="Instagram"
+          />
+          {data.location}
+        </li>
+      </ul>
+    </div>
+    <div class="order-first flex flex-col items-center gap-4 md:order-last">
+      <img src={data.image} width="250" height="250" alt={data.title} />
+      <Button
+        class="hidden border border-white bg-primary/50 px-12 py-6 font-bold uppercase md:flex"
+      >
+        Inscrever
+      </Button>
+    </div>
   </div>
-  <div class="flex flex-col items-center gap-4">
-    <img src={data.image} width="250" height="250" alt={data.title} />
-    <Button class="bg-primary/50 border border-white px-12 py-6 font-bold uppercase"
-      >Inscrever</Button
-    >
-  </div>
+  <Button class="w-fit border border-white bg-primary/50 px-12 py-6 font-bold uppercase md:hidden">
+    Inscrever
+  </Button>
 </div>
