dbf3dfaee7bec52019ad2de247f127830d963b88
Rubem Neto <rubemviscard2635@gmail.com>
2024-08-31 10:10:27 +0100

    Acessability Change

--- src/lib/components/icons/label-input.svelte
+++ src/lib/components/icons/label-input.svelte
@@ -9,12 +9,14 @@
 <label class="m-1 font-source_code font-bold text-white" for={id}>{label}</label><br class="mb-1" />
 {#if isTextArea}
   <textarea
+    aria-label="textarea-input"
     class="mb-2 w-full rounded-lg p-2 font-source_code text-primary placeholder-primary"
     {id}
     rows="4"
   /><br />
 {:else}
   <input
+    aria-label="text-input"
     class="mb-2 w-full rounded-lg p-2 text-primary placeholder-primary"
     {type}
     {id}
