c323102770d62936d1073f28d2e1df0bb8da2be8
Lauranea <93668178+Lauranea@users.noreply.github.com>
2024-11-16 17:40:45 +0000

    added more padding

--- src/lib/components/forms/picture-input.svelte
+++ src/lib/components/forms/picture-input.svelte
@@ -5,22 +5,22 @@
 	let  avatar, fileinput;
 	
 	const onFileSelected =(e)=>{
-            let image = e.target.files[0];
-            let reader = new FileReader();
-            reader.readAsDataURL(image);
-            reader.onload = e => {
-                 avatar = e.target.result
-            };
-}
-	
+        let image = e.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = e => {
+            avatar = e.target.result;    
+        };
+    }
 </script>
+
 <div id="app" class="flex items-center justify-center flex-col">
   
         {#if avatar}
         <div class="flex h-[200px] w-[200px] rounded-md overflow-hidden relative">
             <img class="h-[200px] w-[200px]" src="{avatar}" alt="Avatar" />
             <button type="button" aria-label="Change Avatar" class="bg-rose-950 rounded-md h-[15%] w-[15%] flex items-center justify-center cursor-pointer absolute bottom-0 right-0 m-2" on:click={() => { fileinput.click(); }} on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') { fileinput.click(); } }}>
-                <Icon src={Icons.Edit} color="white" size="80%" />
+                <Icon src={Icons.Edit} color="white" size="70%" />
             </button>
             <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e) => onFileSelected(e)} bind:this={fileinput}>
         </div>
@@ -28,7 +28,7 @@
         <div class="bg-muted-red-400 flex items-center justify-center rounded-md h-[200px] w-[200px] text-center relative">
             <p class="text-white font-medium">Adicionar logo <span class="text-4xl">*</span></p>
             <button type="button" aria-label="Change Avatar" class="bg-rose-950 rounded-md h-[15%] w-[15%] flex items-center justify-center cursor-pointer absolute bottom-0 right-0 m-2" on:click={() => { fileinput.click(); }} on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') { fileinput.click(); } }}>
-                <Icon src={Icons.Edit} color="white" size="80%" />
+                <Icon src={Icons.Edit} color="white" size="60%" />
             </button>
             <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e) => onFileSelected(e)} bind:this={fileinput}>
         </div>
