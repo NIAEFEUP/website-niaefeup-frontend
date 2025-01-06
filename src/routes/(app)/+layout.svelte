dec93e253be16bb6f6eb13b84a44ecdc9aa8806c
Pedro Oliveira <pedromartaoliveira@gmail.com>
2025-01-06 04:03:17 +0000

    Fixed bug regarding browser autofill login options

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -32,4 +32,12 @@
     flex-direction: column;
     overflow: auto;
   }
+
+  :global(input:-webkit-autofill,input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active) {
+    -webkit-box-shadow: 0 0 0px 1000px rgb(255 233 233 / var(--tw-bg-opacity)) inset;   /*Since we can't alter the browsers background change we add an inner shadow with the same defaul color*/ 
+    -webkit-text-fill-color: rgb(65 19 21 / .54);
+    /*.54 should be replaced with var(--tw-bg-opacity) but at least my PC recognized the variable as 1 instead of .54*/
+    /*Possible problem: all input field when autofilled will have this change, even outside of login*/
+  }
+  
 </style>
