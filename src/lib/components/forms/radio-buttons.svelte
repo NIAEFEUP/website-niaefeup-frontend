ed05f356b68e26e77e8b1e4844445977d5b2caf5
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-01-10 13:29:53 +0000

    fix(radio button): fix incorrect label binding

--- src/lib/components/forms/radio-buttons.svelte
+++ src/lib/components/forms/radio-buttons.svelte
@@ -14,8 +14,8 @@
       value={option}
     />
     <label
-      class="m-1 justify-self-start rounded-lg bg-taupe-200 px-5 py-1 text-rose-950 font-bold"
-      for="radio-{option}"
+      class="m-1 justify-self-start rounded-lg bg-taupe-200 px-5 py-1 font-bold text-rose-950"
+      for="radio-{label.toLowerCase()}-{option.toLowerCase()}"
     >
       {option}
     </label>
