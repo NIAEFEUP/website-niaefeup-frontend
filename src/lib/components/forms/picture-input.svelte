f4e8a8ce566886f4610fb7f2b5eaa94040d9209f
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-01-18 16:16:28 +0000

    refactor(picture input): code cleanup

--- src/lib/components/forms/picture-input.svelte
+++ src/lib/components/forms/picture-input.svelte
@@ -3,41 +3,34 @@
   import Icons from '$lib/components/icons/icons';
 
   export let text: string;
-  let avatar: string;
-  let fileinput: HTMLInputElement;
+  let image: string;
+  let fileInput: HTMLInputElement;
 
   const onFileSelected = (e) => {
-    let image = e.target.files[0];
+    let file = e.target.files[0];
     let reader = new FileReader();
-    reader.readAsDataURL(image);
+    reader.readAsDataURL(file);
     reader.onload = (e) => {
-      if (e.target?.result) {
-        avatar = e.target.result.toString();
-      }
+      image = e.target?.result?.toString() ?? image;
     };
   };
 </script>
 
-<div id="app" class="flex flex-col items-center justify-center">
+<div class="flex flex-col items-center justify-center">
   <div
     class="relative flex h-[200px] w-[200px] items-center justify-center rounded-md bg-muted-red-400 text-center"
   >
-    {#if avatar}
-      <img class="h-[200px] w-[200px] object-cover" src={avatar} alt="Avatar" />
+    {#if image}
+      <img class="h-[200px] w-[200px] object-cover" src={image} alt="Avatar" />
     {:else}
       <p class="font-medium text-white">{text}<span class="text-4xl">*</span></p>
     {/if}
     <button
       type="button"
-      aria-label="Change Avatar"
+      aria-label="Upload image"
       class="absolute bottom-0 right-0 m-2 flex h-[15%] w-[15%] cursor-pointer items-center justify-center rounded-md bg-rose-950"
       on:click={() => {
-        fileinput.click();
-      }}
-      on:keydown={(e) => {
-        if (e.key === 'Enter' || e.key === ' ') {
-          fileinput.click();
-        }
+        fileInput.click();
       }}
     >
       <Icon src={Icons.Edit} color="white" size="60%" />
@@ -47,7 +40,7 @@
       type="file"
       accept=".jpg, .jpeg, .png"
       on:change={(e) => onFileSelected(e)}
-      bind:this={fileinput}
+      bind:this={fileInput}
     />
   </div>
 </div>
