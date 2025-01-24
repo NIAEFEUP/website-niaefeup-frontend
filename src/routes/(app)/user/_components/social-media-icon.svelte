0a72261537c18395b920b5b261ed4be5ed4c3ada
Pedro Castro <falecompedroac@gmail.com>
2025-01-24 13:07:54 +0000

    solved accessibility violation

--- src/routes/(app)/user/_components/social-media-icon.svelte
+++ src/routes/(app)/user/_components/social-media-icon.svelte
@@ -10,9 +10,8 @@
 
 <div
   class="h-12 w-12 rounded-md bg-white/20 p-3 sm:h-14 sm:w-14 sm:p-4"
-  aria-label="{user}'s {social}"
 >
-  <a href={url}>
+  <a href={url} aria-label="{user}'s {social}">
     {#if iconPath}
       <img src={iconPath} alt="Icon of {user}'s {social}" class="icon h-full w-full object-cover" />
     {:else}
