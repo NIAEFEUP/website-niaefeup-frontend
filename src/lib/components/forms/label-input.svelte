30d223737a102243996fe1045c1189b26b6c8b64
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-01-25 01:15:13 +0000

    feat(label input): align label with textbox

--- src/lib/components/forms/label-input.svelte
+++ src/lib/components/forms/label-input.svelte
@@ -4,27 +4,28 @@
   export let type = 'text';
   export let placeholder = '';
   export let isTextArea = false;
+  export let horizontal = false;
 </script>
 
-{#if label}
-  <label class="m-1 font-source_code font-bold text-white" for={id}>{label}</label><br
-    class="mb-1"
-  />
-{/if}
-{#if isTextArea}
-  <textarea
-    aria-label="textarea-input"
-    class="mb-2 min-h-[100px] w-full rounded-lg bg-white p-2 font-source_code text-primary placeholder-primary"
-    {id}
-    {placeholder}
-    rows="4"
-  /><br />
-{:else}
-  <input
-    aria-label="text-input"
-    class="mb-2 w-full rounded-lg bg-white p-2 text-primary placeholder-primary"
-    {type}
-    {id}
-    {placeholder}
-  /><br />
-{/if}
+<div class="flex flex-{horizontal ? 'row' : 'col'}">
+  {#if label}
+    <label class="m-1 font-source_code font-bold text-white" for={id}>{label}</label>
+  {/if}
+  {#if isTextArea}
+    <textarea
+      aria-label="textarea-input"
+      class="mb-2 min-h-[100px] w-full rounded-lg bg-white p-2 font-source_code text-primary placeholder-primary"
+      {id}
+      {placeholder}
+      rows="4"
+    />
+  {:else}
+    <input
+      aria-label="text-input"
+      class="mb-2 w-full rounded-lg bg-white p-2 text-primary placeholder-primary"
+      {type}
+      {id}
+      {placeholder}
+    />
+  {/if}
+</div>
