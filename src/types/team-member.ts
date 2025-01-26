f41c8172185820a3c953020e5a3a13637be753ed
Pedro Castro <falecompedroac@gmail.com>
2025-01-26 18:30:38 +0000

    renamed TeamMember gitHub attribute to github

--- src/types/team-member.ts
+++ src/types/team-member.ts
@@ -3,9 +3,11 @@ import type { CustomWebsite } from './custom-website';
 export type TeamMember = {
   name: string;
   email: string;
+  bio: string;
+  birthDate: Date,
   role: string;
   photo?: string;
   linkedin?: string;
-  gitHub?: string;
+  github?: string;
   websites?: CustomWebsite[];
 };
