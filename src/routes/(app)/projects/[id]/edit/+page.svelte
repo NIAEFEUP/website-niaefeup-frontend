664a2cb13b3454bc57dfdda5ee52b61633c82d1e
DaltonDjoesman <daltonyuiti@hotmail.com>
2025-01-26 15:36:50 +0000

    grid update for the info form page

--- src/routes/(app)/projects/[id]/edit/+page.svelte
+++ src/routes/(app)/projects/[id]/edit/+page.svelte
@@ -1,154 +1,161 @@
-<div class = "flex  flex-col items-center">
-    <div class="flex w-1/2">
-        <h1 class = "text-4xl font-bold">
-            Editar projeto
-        </h1>
-    </div>
-    <div class="h-1 w-[50vw] mt-5 bg-red-500"></div>
-</div>
-
-<div class ="flex flex-row justify-center flex-wrap p-10">
-    <div class="flex w-[50vw] h-[100vw] flex-col items-center">
-        <div class = "flex h-10 w-[50vw] items-center">
-            <div class="flex items-center w-1/6">
-                <p class="font-bold">
-                    Nome*
-                </p>
-            </div>
-            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
+<div class="flex items-center flex-col">
+    <div class="flex items-center w-[65vw] flex-col">
+        <div class="flex w-full items-center justify-center border-b-4 border-red-500">    
+            <div class="flex h-[3vw] ">
+                <h1 class = "text-4xl font-bold">
+                    Editar projeto
+                </h1>
             </div>
         </div>
-        <div class = "flex h-10 w-[50vw] mt-5 items-center">
-            <div class="flex items-center w-1/6">
-                <p class="font-bold">
-                    Github*
-                </p>
-            </div>
-            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
-            </div>
-        </div>
-        <div class= "flex w-[50vw] mt-5 h-20">
-            <div class="flex items-center w-1/6">
-                <p class="font-bold">
-                    Slogan*
-                </p>
-            </div>
-            <div class="h-20 w-[30vw] bg-white opacity-70 rounded-xl">
-            </div>
-        </div>
-        <div class = "flex mt-5 w-[50vw]">
-            <div class = "flex w-1/6" >
-                <p class = "font-bold">
-                    Tipo*
-                </p>
-            </div>
-            <div class="flex-col">
-                <div class = "flex gap-10">
-                    <div class = "h-10 w-[9vw] bg-red-500 rounded-xl flex items-center justify-center">
-                        <p>
-                            aplicacao
-                        </p>
-                    </div>
-                    <div class = "h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center" >
-                        <p class="text-black">
-                            website
-                        </p>
-                    </div>
-                    <div class = "h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center">
-                        <p class="text-black">
-                            outro
-                        </p>
-                    </div>
-                </div>
-                <div class = "h-40 bg-white bg-opacity-30 rounded-xl mt-5 flex flex-col items-center justify-center">
-                    <div class = "flex w-[25vw]">
-                        <div class="flex items-center w-1/4">
-                            <p>
-                                Google Play
-                            </p>
-                        </div>
-                        <div class = "h-10 w-[20vw] bg-white rounded-xl">
-                        </div>
-                    </div>
-                    <div class="flex mt-5 w-[25vw]">
-                        <div class="flex items-center w-1/4">
-                            <p>
-                                App Store
-                            </p>
-                        </div>
-                        <div class="h-10 w-[20vw] bg-white rounded-xl">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="flex w-[50vw] mt-5">
-            <div class="flex w-1/6">
-                <p class="font-bold">
-                    Descrição
-                </p>
-            </div>
-            <div class="h-40 w-[30vw] bg-white opacity-70 rounded-xl">
-            </div>
-        </div>
-        <div class="flex h-10 w-[50vw] mt-5">
-            <div class="flex items-center w-1/6">
-                <p class="font-bold">
-                    Fotos
-                </p>
-            </div>
-            <div class="flex gap-10">
-                <div class="h-10 w-[9vw] bg-red-500 rounded-xl flex items-center justify-center">
-                    <p>
-                        Selecionar Ficheiro
+            <!-- <div class="h-1 w-[70vw] mt-5 bg-red-500"></div> -->
+        <div class ="flex flex-col justify-center flex-wrap p-10">
+                <div class="flex items-center justify-center">
+                    <p class="text-2xl font-bold">
+                        Dados Gerais
                     </p>
                 </div>
-                <div class="h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center">
-                    <p class="text-black">
-                        Image.png
-                    </p>
+                <div class="flex flex-row items-center p-10">  
+                    <div>
+                        <div class = "flex h-10 w-[50vw] items-center">
+                            <div class="flex items-center w-1/6">
+                                <p class="font-bold">
+                                    Nome*
+                                </p>
+                            </div>
+                            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class = "flex h-10 w-[50vw] mt-5 items-center">
+                            <div class="flex items-center w-1/6">
+                                <p class="font-bold">
+                                    Github*
+                                </p>
+                            </div>
+                            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class= "flex w-[50vw] mt-5 h-20">
+                            <div class="flex items-center w-1/6">
+                                <p class="font-bold">
+                                    Slogan*
+                                </p>
+                            </div>
+                            <div class="h-20 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class = "flex mt-5 w-[50vw]">
+                            <div class = "flex w-1/6" >
+                                <p class = "font-bold">
+                                    Tipo*
+                                </p>
+                            </div>
+                            <div class="flex-col">
+                                <div class = "flex gap-10">
+                                    <div class = "h-10 w-[9vw] bg-red-500 rounded-xl flex items-center justify-center">
+                                        <p>
+                                            aplicacao
+                                        </p>
+                                    </div>
+                                    <div class = "h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center" >
+                                        <p class="text-black">
+                                            website
+                                        </p>
+                                    </div>
+                                    <div class = "h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center">
+                                        <p class="text-black">
+                                            outro
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class = "h-40 bg-white bg-opacity-30 rounded-xl mt-5 flex flex-col items-center justify-center">
+                                    <div class = "flex w-[25vw]">
+                                        <div class="flex items-center w-1/4">
+                                            <p>
+                                                Google Play
+                                            </p>
+                                        </div>
+                                        <div class = "h-10 w-[20vw] bg-white rounded-xl">
+                                        </div>
+                                    </div>
+                                    <div class="flex mt-5 w-[25vw]">
+                                        <div class="flex items-center w-1/4">
+                                            <p>
+                                                App Store
+                                            </p>
+                                        </div>
+                                        <div class="h-10 w-[20vw] bg-white rounded-xl">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex w-[50vw] mt-5">
+                            <div class="flex w-1/6">
+                                <p class="font-bold">
+                                    Descrição
+                                </p>
+                            </div>
+                            <div class="h-40 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class="flex h-10 w-[50vw] mt-5">
+                            <div class="flex items-center w-1/6">
+                                <p class="font-bold">
+                                    Fotos
+                                </p>
+                            </div>
+                            <div class="flex gap-10">
+                                <div class="h-10 w-[9vw] bg-red-500 rounded-xl flex items-center justify-center">
+                                    <p>
+                                        Selecionar Ficheiro
+                                    </p>
+                                </div>
+                                <div class="h-10 w-[9vw] bg-white rounded-xl flex items-center justify-center">
+                                    <p class="text-black">
+                                        Image.png
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex h-10 w-[50vw] mt-5">
+                            <div class="flex items-center w-1/6">
+                                <p class="font-bold">
+                                    Público Alvo
+                                </p>
+                            </div>
+                            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class="flex w-[50vw] mt-5">
+                            <div class="flex w-1/6">
+                                <p class="font-bold">
+                                    Tecnologias
+                                </p>
+                            </div>
+                            <div class="h-40 w-[30vw] bg-white opacity-70 rounded-xl">
+                            </div>
+                        </div>
+                        <div class="flex w-[50vw] mt-[5vw] gap-10">
+                            <div class="h-10 w-[15vw] bg-red-500 rounded-xl flex items-center justify-center">
+                                <p class="font-bold">
+                                    Guardar Alterações
+                                </p>
+                            </div>
+                            <div class="h-10 w-[10vw] bg-gray-500 rounded-xl flex items-center justify-center">
+                                <p class="font-bold">
+                                  Cancelar  
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex self-start">
+                        <div class="h-[10vw] w-[10vw] bg-red-500 bg-opacity-80 rounded-xl flex items-center justify-center">
+                            <p class= "font-bold">
+                                Adicionar Logo*
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="flex h-10 w-[50vw] mt-5">
-            <div class="flex items-center w-1/6">
-                <p class="font-bold">
-                    Público Alvo
-                </p>
-            </div>
-            <div class="h-10 w-[30vw] bg-white opacity-70 rounded-xl">
-            </div>
-        </div>
-        <div class="flex w-[50vw] mt-5">
-            <div class="flex w-1/6">
-                <p class="font-bold">
-                    Tecnologias
-                </p>
-            </div>
-            <div class="h-40 w-[30vw] bg-white opacity-70 rounded-xl">
-            </div>
-        </div>
-        <div class="flex w-[50vw] mt-[10vw] gap-10">
-            <div class="h-10 w-[15vw] bg-red-500 rounded-xl flex items-center justify-center">
-                <p class="font-bold">
-                    Guardar Alterações
-                </p>
-            </div>
-            <div class="h-10 w-[10vw] bg-gray-500 rounded-xl flex items-center justify-center">
-                <p class="font-bold">
-                  Cancelar  
-                </p>
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="flex">
-        <div class="h-[10vw] w-[10vw] bg-red-500 bg-opacity-80 rounded-xl flex items-center justify-center">
-            <p class= "font-bold">
-                Adicionar Logo*
-            </p>
         </div>
     </div>
 </div>
