398b452da1b3af9fc4013d0784ac1fc52d903ce4
Isaac3105 <isaac.absantos@gmail.com>
2025-02-12 11:19:32 +0000

    details

--- src/routes/(app)/events/create/+page.svelte
+++ src/routes/(app)/events/create/+page.svelte
@@ -1,62 +1,52 @@
 <div>
-  <div class="flex flex-row">
-    <p class="ml-11 text-5xl font-bold">Novo Evento</p>
-  </div>
-
-  <div class="mx-auto my-8 h-1 w-[97%] bg-red-500"></div>
-
-  <div class="mx-11 flex flex-row gap-8">
-    <div class="flex flex-col font-semibold">
-      <div class="my-6 ml-48 flex flex-col gap-14">
-        <p>Título*</p>
-        <p>Data / Hora*</p>
-        <p>Localização*</p>
-        <p>Descrição</p>
-      </div>
-
-      <div class="my-16 ml-48 flex flex-col gap-14">
-        <p class="">Inscrição</p>
-        <p>Fotos</p>
-        <p>Patrocinadores</p>
-      </div>
+    
+    <div class="flex flex-row">
+        <p class="font-bold text-5xl ml-11">
+            Novo Evento
+        </p>
     </div>
 
-    <div class="mx-10 my-5 flex flex-col gap-9">
-      <div class="h-10 w-[700px] rounded-lg border border-gray-300 bg-white shadow-sm"></div>
-      <div class="h-10 w-[700px] rounded-lg border border-gray-300 bg-white shadow-sm"></div>
-      <div class="h-10 w-[700px] rounded-lg border border-gray-300 bg-white shadow-sm"></div>
-      <div class="h-20 w-[700px] rounded-lg border border-gray-300 bg-white shadow-sm"></div>
-      <div class="h-10 w-[700px] rounded-lg border border-gray-300 bg-white shadow-sm"></div>
-      <div class="flex flex-row gap-16">
-        <div class="flex h-10 w-48 items-center justify-center rounded-lg bg-red-400 font-semibold">
-          Selecionar Ficheiros
-        </div>
-        <div
-          class="flex h-10 w-40 items-center justify-center rounded-lg bg-taupe-200 font-semibold"
-        >
-          image.png
-        </div>
-        <div
-          class="flex h-10 w-40 items-center justify-center rounded-lg bg-taupe-200 font-semibold"
-        >
-          image.png
-        </div>
-      </div>
+    <div class="my-8 mx-auto w-[97%] h-1 bg-red-500"></div>
 
-      <div class="my-3 flex flex-row gap-16">
-        <div class="flex h-10 w-48 items-center justify-center rounded-lg bg-red-400 font-semibold">
-          Selecionar Ficheiros
-        </div>
-        <p class="flex items-center justify-center font-semibold">Nenhum Ficheiro Selecionado</p>
-      </div>
-    </div>
+    <div class="flex flex-row gap-8 mx-11">
 
-    <div class="mx-32 my-20 flex flex-col">
-      <div
-        class="flex h-60 w-60 items-center justify-center rounded-lg bg-red-400 text-2xl font-semibold"
-      >
-        Adicionar Foto*
-      </div>
+        <div class="flex flex-col font-semibold">
+            <div class = "flex flex-col ml-48 my-6 gap-14">
+                <p>Título*</p>
+                <p>Data / Hora*</p>
+                <p>Localização*</p>
+                <p >Descrição</p>
+            </div>
+
+            <div class = "flex flex-col ml-48 my-16 gap-14">
+                <p class = "">Inscrição</p>
+                <p>Fotos</p>
+                <p>Patrocinadores</p>
+            </div>
+
+        </div>
+
+        <div class="flex flex-col gap-9 my-5 mx-10">
+            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
+            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
+            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
+            <div class="w-[700px] h-20 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
+            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
+            <div class = "flex flex-row gap-16">
+
+                <div class="w-48 h-10 bg-red-400 rounded-lg flex items-center justify-center font-semibold">Selecionar Ficheiros</div>
+                <div class="w-40 h-10 bg-taupe-200 rounded-lg flex items-center justify-center font-semibold">image.png</div>
+                <div class="w-40 h-10 bg-taupe-200 rounded-lg flex items-center justify-center font-semibold">image.png</div>
+            </div>
+
+            <div class = "flex flex-row my-3 gap-16">
+                <div class="w-48 h-10 bg-red-400 rounded-lg flex items-center justify-center font-semibold">Selecionar Ficheiros</div>
+                <p class = "font-semibold flex items-center justify-center">Nenhum Ficheiro Selecionado</p>
+            </div>
+        </div>
+
+        <div class="flex flex-col mx-32 my-20">
+            <div class = "w-60 h-60 bg-red-400 rounded-lg flex items-center justify-center font-semibold text-2xl">Adicionar Foto*</div>
+        </div>
     </div>
-  </div>
 </div>
