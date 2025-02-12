f8f5f02a095ac38f73c0b9906cf68ebd11ba272c
Pedro Castro <falecompedroac@gmail.com>
2025-02-12 16:16:31 +0000

    integrated backend's isActive flag into the user page

--- src/routes/(app)/user/[id]/+page.svelte
+++ src/routes/(app)/user/[id]/+page.svelte
@@ -53,11 +53,17 @@
               <h3 class="text-2xl font-bold lg:text-lg xl:text-xl 2xl:text-2xl">
                 {teamMember.name}
               </h3>
-              <div class="flex items-center rounded-2xl bg-green-200/20 px-5 py-2">
-                <div class="mr-2 h-4 w-4 rounded-full bg-green-200"></div>
-                <!-- TODO: incorporate backend's active member flag -->
-                <p class="text-sm font-semibold sm:text-base">Membro ativo</p>
-              </div>
+              {#if teamMember.isActive}
+                <div class="flex items-center rounded-2xl bg-green-200/20 px-5 py-2">
+                  <div class="mr-2 h-4 w-4 rounded-full bg-green-200"></div>
+                  <p class="text-sm font-semibold sm:text-base">Membro ativo</p>
+                </div>
+              {:else}
+                <div class="flex items-center rounded-2xl bg-red-200/20 px-5 py-2">
+                  <div class="mr-2 h-4 w-4 rounded-full bg-red-500"></div>
+                  <p class="text-sm font-semibold sm:text-base">Membro não ativo</p>
+                </div>
+              {/if}
             </div>
             {#if teamMember.bio}
               <p
