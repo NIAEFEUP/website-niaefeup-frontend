f3c8512fa8c29b9bf052ec5a9ea97376272798fb
Pedro Castro <falecompedroac@gmail.com>
2025-02-13 13:55:52 +0000

    added EditButton storybook

---
+++ src/lib/components/edit-button.stories.ts
@@ -0,0 +1,31 @@
+import EditButton from './edit-button.svelte';
+
+export default {
+  title: 'Atoms/Buttons/EditButton',
+  component: EditButton,
+  parameters: {
+    layout: 'centered',
+    controls: { exclude: ['type'] }
+  }
+};
+
+export const SmallEditButton = {
+  args: {
+    size: 'small',
+    link: 'https://lipsum.org'
+  }
+};
+
+export const MediumEditButton = {
+  args: {
+    size: 'medium',
+    link: 'https://lipsum.org'
+  }
+};
+
+export const LargeEditButton = {
+  args: {
+    size: 'large',
+    link: 'https://lipsum.org'
+  }
+};
