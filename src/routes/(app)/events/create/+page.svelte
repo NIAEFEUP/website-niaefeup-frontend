aa09eddb3fb994d82ca213a08a886c0b0760aae7
Isaac3105 <isaac.absantos@gmail.com>
2025-02-17 22:03:25 +0000

    Trying to rebase

--- src/routes/(app)/events/create/+page.svelte
+++ src/routes/(app)/events/create/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+    import PictureInput from '$lib/components/forms/picture-input.svelte';
+    import LabelInput from '$lib/components/forms/label-input.svelte';
+</script>
+
+
+
 <div>
     
     <div class="flex flex-row">
@@ -8,37 +15,15 @@
 
     <div class="my-8 mx-auto w-[97%] h-1 bg-red-500"></div>
 
-    <div class="flex flex-row gap-8 mx-11">
+    <div class="flex flex-row gap-16 mx-20 justify-center items-center ">
 
-        <div class="flex flex-col font-semibold">
-            <div class = "flex flex-col ml-48 my-6 gap-14">
-                <p>Título*</p>
-                <p>Data / Hora*</p>
-                <p>Localização*</p>
-                <p >Descrição</p>
-            </div>
-
-            <div class = "flex flex-col ml-48 my-16 gap-14">
-                <p class = "">Inscrição</p>
-                <p>Fotos</p>
-                <p>Patrocinadores</p>
-            </div>
-
-        </div>
-
-        <div class="flex flex-col gap-9 my-5 mx-10">
-            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
-            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
-            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
-            <div class="w-[700px] h-20 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
-            <div class="w-[700px] h-10 bg-white rounded-lg border border-gray-300 shadow-sm"></div>
-            <div class = "flex flex-row gap-16">
-
-                <div class="w-48 h-10 bg-red-400 rounded-lg flex items-center justify-center font-semibold">Selecionar Ficheiros</div>
-                <div class="w-40 h-10 bg-taupe-200 rounded-lg flex items-center justify-center font-semibold">image.png</div>
-                <div class="w-40 h-10 bg-taupe-200 rounded-lg flex items-center justify-center font-semibold">image.png</div>
-            </div>
-
+        <div class="flex flex-col gap-8 font-semibold">
+            
+            <LabelInput label = "Título*" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
+            <LabelInput label = "Data / Hora*" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
+            <LabelInput label = "Localização*" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
+            <LabelInput label = "Descrição" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
+            <LabelInput label = "Inscrição" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
             <div class = "flex flex-row my-3 gap-16">
                 <div class="w-48 h-10 bg-red-400 rounded-lg flex items-center justify-center font-semibold">Selecionar Ficheiros</div>
                 <p class = "font-semibold flex items-center justify-center">Nenhum Ficheiro Selecionado</p>
@@ -46,7 +31,7 @@
         </div>
 
         <div class="flex flex-col mx-32 my-20">
-            <div class = "w-60 h-60 bg-red-400 rounded-lg flex items-center justify-center font-semibold text-2xl">Adicionar Foto*</div>
+            <PictureInput text="Insira a Imagem" name="Profile Picture"/>
         </div>
     </div>
 </div>
