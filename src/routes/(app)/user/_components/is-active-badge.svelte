80a942bd11d1dbfd3a5f1c38d2fa3ba71d9189f2
Pedro Castro <falecompedroac@gmail.com>
2025-02-17 22:48:51 +0000

    added IsActiveBadge storybook

--- src/routes/(app)/user/_components/is-active-badge.svelte
+++ src/routes/(app)/user/_components/is-active-badge.svelte
@@ -8,12 +8,12 @@
   {#if teamMember.isActive}
     <div class="flex items-center rounded-2xl bg-green-200/20 px-5 py-2">
       <div class="mr-2 h-4 w-4 rounded-full bg-green-200"></div>
-      <p class="text-sm font-semibold sm:text-base">Membro ativo</p>
+      <p class="text-sm font-semibold text-white sm:text-base">Membro ativo</p>
     </div>
   {:else}
     <div class="flex items-center rounded-2xl bg-red-200/20 px-5 py-2">
       <div class="mr-2 h-4 w-4 rounded-full bg-red-500"></div>
-      <p class="text-sm font-semibold sm:text-base">Membro não ativo</p>
+      <p class="text-sm font-semibold text-white sm:text-base">Membro não ativo</p>
     </div>
   {/if}
 </div>
