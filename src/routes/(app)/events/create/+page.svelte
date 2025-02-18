50571971d25d276f2f5e85fc48700455282604a3
Isaac3105 <isaac.absantos@gmail.com>
2025-02-18 17:08:34 +0000

    lint

--- src/routes/(app)/events/create/+page.svelte
+++ src/routes/(app)/events/create/+page.svelte
@@ -1,39 +1,70 @@
-
 <script>
-    import PictureInput from '$lib/components/forms/picture-input.svelte';
-    import LabelInput from '$lib/components/forms/label-input.svelte';
+  import PictureInput from '$lib/components/forms/picture-input.svelte';
+  import LabelInput from '$lib/components/forms/label-input.svelte';
 </script>
 
-
-
 <div>
-    
-    <div class="flex flex-row">
-        <p class="font-bold text-5xl ml-11">
-            Novo Evento
-        </p>
+  <div class="flex flex-row">
+    <p class="ml-11 text-5xl font-bold">Novo Evento</p>
+  </div>
+
+  <div class="mx-auto my-8 h-1 w-[97%] bg-red-500"></div>
+
+  <div class="flex flex-row">
+    <div class="ml-60 mr-40 mt-10 flex flex-col gap-10 font-semibold">
+      <LabelInput
+        label="Título*"
+        id="caixa de texto1"
+        type="text"
+        placeholder="Insira o texto"
+        isTextArea={false}
+        horizontal={true}
+      />
+      <LabelInput
+        label="Data / Hora*"
+        id="caixa de texto2"
+        type="text"
+        placeholder="Insira o texto"
+        isTextArea={false}
+        horizontal={true}
+      />
+      <LabelInput
+        label="Localização*"
+        id="caixa de texto3"
+        type="text"
+        placeholder="Insira o texto"
+        isTextArea={false}
+        horizontal={true}
+      />
+      <LabelInput
+        label="Descrição"
+        id="caixa de texto4"
+        type="text"
+        placeholder="Insira o texto"
+        isTextArea={false}
+        horizontal={true}
+      />
+      <LabelInput
+        label="Inscrição"
+        id="caixa de texto5"
+        type="text"
+        placeholder="Insira o texto"
+        isTextArea={false}
+        horizontal={true}
+      />
+      <div class="mt-5 flex flex-row gap-16">
+        <div class="flex h-10 w-48 items-center justify-center rounded-lg bg-red-400 font-semibold">
+          Selecionar Ficheiros
+        </div>
+        <p class="flex items-center justify-center font-semibold">Nenhum Ficheiro Selecionado</p>
+      </div>
+      <div class="flex h-12 w-48 items-center justify-center rounded-lg bg-red-500 font-bold">
+        Guardar Alterações
+      </div>
     </div>
 
-    <div class="my-8 mx-auto w-[97%] h-1 bg-red-500"></div>
-
-    <div class="flex flex-row">
-
-        <div class="flex flex-col gap-10 ml-60 mr-40 mt-10 font-semibold">
-            
-            <LabelInput label = "Título*" id = "caixa de texto1" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
-            <LabelInput label = "Data / Hora*" id = "caixa de texto2" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
-            <LabelInput label = "Localização*" id = "caixa de texto3" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
-            <LabelInput label = "Descrição" id = "caixa de texto4" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
-            <LabelInput label = "Inscrição" id = "caixa de texto5" type = "text" placeholder = "Insira o texto" isTextArea = {false} horizontal = {true}/>
-            <div class = "flex flex-row mt-5 gap-16">
-                <div class="w-48 h-10 bg-red-400 rounded-lg flex items-center justify-center font-semibold">Selecionar Ficheiros</div>
-                <p class = "font-semibold flex items-center justify-center">Nenhum Ficheiro Selecionado</p>
-            </div>
-            <div class="w-48 h-12 bg-red-500 rounded-lg flex items-center justify-center font-bold">Guardar Alterações</div>
-        </div>
-
-        <div class="flex flex-col ml-80 mt-20">
-            <PictureInput text="Insira a Imagem" name="Profile Picture"/>
-        </div>
+    <div class="ml-80 mt-20 flex flex-col">
+      <PictureInput text="Insira a Imagem" name="Profile Picture" />
     </div>
+  </div>
 </div>
