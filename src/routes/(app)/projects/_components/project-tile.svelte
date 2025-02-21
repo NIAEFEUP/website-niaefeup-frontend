62145e97e9689ca8fbdefc20d8ac607353acc0ff
Pedro Castro <falecompedroac@gmail.com>
2025-02-21 14:29:20 +0000

    fixed formatting

--- src/routes/(app)/projects/_components/project-tile.svelte
+++ src/routes/(app)/projects/_components/project-tile.svelte
@@ -1,33 +1,45 @@
 <script lang="ts">
-    let windowWidth: number;
+  let windowWidth: number;
 
-    export let
-        name: string;
-    const colorList = {
-        nijobs: {
-            background: 'bg-[#9c342f]',
-            text: 'text-gray-200'
-        },
-        uni: {
-            background:'bg-[#521015]',
-            text: 'text-gray-200'
-        },
-        tts: {
-            background: 'bg-[#A8A8A8]',
-            text: 'text-gray-900'
-        }
-    };
+  export let name: string;
+  const colorList = {
+    nijobs: {
+      background: 'bg-[#9c342f]',
+      text: 'text-gray-200'
+    },
+    uni: {
+      background: 'bg-[#521015]',
+      text: 'text-gray-200'
+    },
+    tts: {
+      background: 'bg-[#A8A8A8]',
+      text: 'text-gray-900'
+    }
+  };
 </script>
 
-<svelte:window bind:innerWidth={windowWidth}></svelte:window>
+<svelte:window bind:innerWidth={windowWidth} />
 
-<div class='{colorList[name.toLowerCase()].background} h-44 w-4/5 rounded-xl flex flex-col justify-center
-    {windowWidth < 768 ? 'odd:self-start even:self-end odd:items-end even:items-start' : null}
-    md:self-center md:w-auto md:h-auto md:flex-none md:bg-transparent '>
-    <div class='flex flex-col items-center py-3 px-4 -mb-3 mx-5 gap-1
-                md:justify-between md:px-0 md:py-0 md:gap-4 md:basis-3/10'>
-        <img class='h-20 shadow-lg rounded-xl md:shadow-none md:h-72' src={'/images/projects/'+ name.toLowerCase() + (windowWidth >= 768 ? '.png' : '_logo.png')} alt='nijobs logo'/>
-        <p class='font-raleway font-semibold text-2xl {colorList[name.toLowerCase()].text}
-                  md:text-white md:text-5xl'>{name}</p>
-    </div>
+<div
+  class="{colorList[name.toLowerCase()]
+    .background} flex h-44 w-4/5 flex-col justify-center rounded-xl
+    {windowWidth < 768 ? 'odd:items-end odd:self-start even:items-start even:self-end' : null}
+    md:h-auto md:w-auto md:flex-none md:self-center md:bg-transparent"
+>
+  <div
+    class="md:basis-3/10 mx-5 -mb-3 flex flex-col items-center gap-1 px-4
+                py-3 md:justify-between md:gap-4 md:px-0 md:py-0"
+  >
+    <img
+      class="h-20 rounded-xl shadow-lg md:h-72 md:shadow-none"
+      src={'/images/projects/' + name.toLowerCase() + (windowWidth >= 768 ? '.png' : '_logo.png')}
+      alt="nijobs logo"
+    />
+    <p
+      class="font-raleway text-2xl font-semibold {colorList[name.toLowerCase()].text}
+                  md:text-5xl md:text-white"
+    >
+      {name}
+    </p>
+  </div>
 </div>
