761340c598c8ca81e7b50d79d31928a1234aa73c
Pedro Castro <falecompedroac@gmail.com>
2025-02-21 15:36:41 +0000

    added Novo Projeto button

--- src/routes/(app)/projects/+page.svelte
+++ src/routes/(app)/projects/+page.svelte
@@ -1,20 +1,30 @@
 <script>
   import ProjectTile from './_components/project-tile.svelte';
+  import Button from '@/lib/components/buttons/button.svelte';
 
   const projects = [{ name: 'NIJobs' }, { name: 'UNI' }, { name: 'TTS' }];
 </script>
 
-<div class="flex flex-col items-center">
-  <h1 class="my-10 font-source_code text-xl text-white md:my-20 md:text-3xl">
-    &lt&nbsp<strong>Projetos</strong>&nbsp/&gt
-  </h1>
+<section>
+  <div class="flex w-full flex-col items-center">
+    <h1 class="my-2 text-center font-source_code text-xl text-white md:my-4 md:text-3xl">
+      &lt&nbsp<strong>Projetos</strong>&nbsp/&gt
+    </h1>
+    <!-- TODO: only show this if the user is authenticated and has the given rights -->
+    <div class="flex w-5/6 justify-end sm:my-6 md:my-6 lg:mb-12 xl:mb-12 2xl:mb-12">
+      <!-- TODO: link the new project page -->
+      <a href="https://lipsum.com">
+        <Button color="secondary" hoverColor="red" width="12" text="Novo Projeto"></Button>
+      </a>
+    </div>
 
-  <div
-    class="-mt-1 flex h-full w-full flex-col flex-wrap justify-evenly gap-4
-                md:mt-0 md:flex-row md:items-end md:gap-x-1.5"
-  >
-    {#each projects as project}
-      <ProjectTile name={project.name} />
-    {/each}
+    <div
+      class="mt-4 flex h-full w-full flex-col flex-wrap justify-evenly gap-4
+                md:mt-6 md:flex-row md:items-end md:gap-x-1.5"
+    >
+      {#each projects as project}
+        <ProjectTile name={project.name} />
+      {/each}
+    </div>
   </div>
-</div>
+</section>
