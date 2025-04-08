55cbcf03f4a28d9cb1efc4eb7a685fcf572adaa2
Pedro Castro <falecompedroac@gmail.com>
2025-04-08 14:28:33 +0100

    perms check

--- src/routes/(app)/projects/+page.svelte
+++ src/routes/(app)/projects/+page.svelte
@@ -6,6 +6,7 @@
 
   export let data: PageData;
   export let projects: Project[] = data.projects;
+  export let hasPerms: boolean = data.hasPerms;
 </script>
 
 <section>
@@ -13,13 +14,14 @@
     <h1 class="my-2 text-center font-source_code text-xl text-white md:my-4 md:text-3xl">
       &lt&nbsp<strong>Projetos</strong>&nbsp/&gt
     </h1>
-    <!-- TODO: only show this if the user is authenticated and has the given rights -->
-    <div class="my-8 flex w-5/6 justify-end sm:my-6 md:my-6 lg:mb-12 xl:mb-12 2xl:mb-12">
-      <!-- TODO: link the new project page -->
-      <a href="https://lipsum.com">
-        <Button color="secondary" hoverColor="red" width="12" text="Novo Projeto"></Button>
-      </a>
-    </div>
+    {#if hasPerms}
+      <div class="my-8 flex w-5/6 justify-end sm:my-6 md:my-6 lg:mb-12 xl:mb-12 2xl:mb-12">
+        <!-- TODO: link the new project page -->
+        <a href="https://lipsum.com">
+          <Button color="secondary" hoverColor="red" width="12" text="Novo Projeto"></Button>
+        </a>
+      </div>
+    {/if}
 
     <div
       class="mt-4 flex h-full w-full flex-col justify-evenly gap-4
--- src/routes/(app)/projects/+page.ts
+++ src/routes/(app)/projects/+page.ts
@@ -2,10 +2,14 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-  const res = await fetch(`/api/projects`);
+  let res = await fetch(`/api/projects`);
   if (!res.ok) error(res.status, 'No projects found');
 
   const projects = await res.json();
 
-  return { projects };
+  let hasPerms = false;
+  res = await fetch(`/api/auth/hasPermission/4`);
+  if (res.status !== 403) hasPerms = true;
+
+  return { projects, hasPerms };
 };
