4eb2267a009d06fcd2e1a4bcf35050626abeb06f
DaltonDjoesman <daltonyuiti@hotmail.com>
2025-04-17 19:27:32 +0100

    obile version update

--- src/routes/(app)/projects/[id]/edit/+page.svelte
+++ src/routes/(app)/projects/[id]/edit/+page.svelte
@@ -38,10 +38,10 @@
                 </div>
                 <div class="mt-5 flex flex-col items-center rounded-xl bg-white bg-opacity-30">
                   <div class="mt-5">
-                    <LabelInput label="Google Play" horizontal labelwidth="1/4" name = "googleplay" placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=15/>
+                    <LabelInput label="Google Play" horizontal labelwidth="1/4" name = "googleplay" placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=10/>
                   </div>
-                  <div class="mt-5">
-                    <LabelInput label="App Store" horizontal boxwidth="[25vw]" labelwidth="1/4" name = "appstore" placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=15/>
+                  <div class="mt-5 pb-5">
+                    <LabelInput label="App Store" horizontal boxwidth="[25vw]" labelwidth="1/4" name = "appstore" placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=10/>
                   </div>
                 </div>
               </div>
@@ -58,14 +58,13 @@
                 <RadioButtons options={['Selecionar ficheiro', 'image.png']} horizontal />
               </div>
             </div>
-
-            <div class="flex md:mt-5 justify-start">
-              <LabelInput label="Público Alvo" name="targetaudience" horizontal required placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=10/>
+            <div class="mt-5">
+              <LabelInput label="Público Alvo" name = "description" horizontal required placeholder="Insira o texto" className="flex-col mr-5 md:flex-row mde:ml-0 md:mr-0" textGap=10 /> <!--se colocar 20 da certo no mobile-->
             </div>
-            <Button type="submit" color="primary" hoverColor="secondary" text="morra" />
             <div class="mt-[5vw] flex w-[50vw] gap-10">
               <RadioButtons options={['Guardar Alteracoes', 'Cancelar']} horizontal />
             </div>
+            <Button type="submit" color="primary" hoverColor="secondary" text="morra" />
           </div>
           <div class="md:self-start order-1 self-center flex flex-col order-1 ">
             <div>
