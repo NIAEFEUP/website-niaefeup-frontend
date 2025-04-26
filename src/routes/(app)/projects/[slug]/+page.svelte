da2e997726949f95612f9f8b1ba5e2e59ad5ae57
Pedro Castro <falecompedroac@gmail.com>
2025-04-26 14:56:05 +0100

    adjusting page styling

--- src/routes/(app)/projects/[slug]/+page.svelte
+++ src/routes/(app)/projects/[slug]/+page.svelte
@@ -21,17 +21,21 @@
     </header>
 
     <div class="mt-28 text-center">
-      <strong class="text-5xl text-white">{project.slogan}</strong>
+      <strong class="text-4xl text-white">{project.slogan}</strong>
     </div>
 
-    <ul class="mt-24 flex justify-center gap-11">
+    <ul class="mt-24 flex justify-center gap-10">
       {#each project.links as link}
         <li>
           <a
             href={link.url}
-            class="flex h-20 w-60 items-center justify-center rounded-xl bg-[#872020] text-white"
+            class="flex h-20 w-60 items-center justify-center overflow-hidden rounded-xl bg-[#872020] text-white"
           >
-            <img src={link.iconPath} alt="{project.title}'s custom website" />
+            <img
+              src={link.iconPath}
+              alt="{project.title}'s custom website"
+              class="max-h-60 object-contain"
+            />
           </a>
         </li>
       {/each}
@@ -46,23 +50,26 @@
 
     <div class="mt-24 flex justify-evenly">
       <div class="flex w-1/3 flex-col items-center">
-        <p class="font-source_code text-5xl font-bold text-white">&lt; Tecnologias /&gt;</p>
-        <div class="mt-6 flex justify-center gap-8">
+        <p class="mb-6 font-source_code text-5xl font-bold text-white">&lt; Tecnologias /&gt;</p>
+        <div class="mt-6 flex flex-wrap justify-center gap-8">
           {#each project.technologies as technology}
             <!-- TODO: fix this when we have the technologies entity implemented in the backend -->
-            <div class="flex gap-4 rounded-full bg-[#674244]/40 px-8 py-4">
+            <div
+              class="flex h-24 w-64 content-center items-center gap-4 rounded-full bg-[#674244]/40 px-8 py-4"
+            >
               <img
                 src="https://picsum.photos/id/239/200/200"
                 alt="Logótipo da tecnologia {technology}"
+                class="max-h-20"
               />
-              <p class="text-2xl text-white">{technology}</p>
+              <p class="text-xl text-white">{technology}</p>
             </div>
           {/each}
         </div>
       </div>
       <div class="flex w-1/3 flex-col items-center">
-        <p class="font-source_code text-5xl font-bold text-white">&lt; Público Alvo /&gt;</p>
-        <div class="mt-6 flex justify-center gap-8">
+        <p class="mb-6 font-source_code text-5xl font-bold text-white">&lt; Público Alvo /&gt;</p>
+        <div class="flex justify-center gap-8">
           <p class="text-center text-3xl font-semibold text-white">
             {project.targetAudience}
           </p>
