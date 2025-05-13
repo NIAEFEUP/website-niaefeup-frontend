e047745ee0750c3055afbf824d14bb93a525fd57
Rita Lopes <93883163+MRita443@users.noreply.github.com>
2025-05-13 19:12:35 +0100

    Update src/routes/api/[...endpoint]/+server.ts
    Co-authored-by: Rubem Neto <88210776+rubuy-74@users.noreply.github.com>

--- src/routes/api/[...endpoint]/+server.ts
+++ src/routes/api/[...endpoint]/+server.ts
@@ -9,7 +9,7 @@ const dispatchToBackend: RequestHandler = async (event) => {
     event.request.method === 'HEAD'
       ? undefined
       : await event.request.body;
-  return fetchWithAuth(event.cookies, endpoint(event.url), event.request.method, undefined, body);
+  return fetchWithAuth(event.cookies, endpoint(event.url), event.request.method, event.request.headers, body);
 };
 
 export const GET: RequestHandler = dispatchToBackend;
