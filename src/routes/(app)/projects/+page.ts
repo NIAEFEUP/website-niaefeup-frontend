c26f13bc285323c6681437530b9398849cc7ca72
Pedro Castro <falecompedroac@gmail.com>
2025-05-20 18:53:23 +0100

    improved fetch error message

--- src/routes/(app)/projects/+page.ts
+++ src/routes/(app)/projects/+page.ts
@@ -3,7 +3,13 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
   let res = await fetch(`/api/projects`);
-  if (!res.ok) error(res.status, 'No projects found');
+  if (!res.ok) {
+    if (res.status === 404) {
+      error(404, 'No projects found');
+    } else {
+      error(res.status, 'Failed to load projects');
+    }
+  }
 
   const projects = await res.json();
 
