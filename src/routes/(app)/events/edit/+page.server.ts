059aa6ae1337aa9eff3dfd1987a0d287fee10d15
Isaac3105 <isaac.absantos@gmail.com>
2025-06-10 10:26:06 +0100

    Tried creating a page for event edition. Don't think if it works tho...

---
+++ src/routes/(app)/events/edit/+page.server.ts
@@ -0,0 +1,58 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+
+import type { PageServerLoad } from '$types';
+
+export const load: PageServerLoad = async ({ params, fetch }) => {
+  const res = await fetch(`/api/events/${params.id}`);
+  const event = await res.json();
+
+  return {
+    event
+  };
+};
+
+
+
+export const actions = {
+  default: async ({ request, fetch }: RequestEvent) => {
+    const data = await request.formData();
+
+    const title = data.get('Title');
+    const slug = data.get('Slug');
+    const description = data.get('Description');
+    const start = new Date(data.get('DateStart') as string);
+    const end = new Date(data.get('DateEnd') as string);
+
+    const startJson = `${start.getDay()}-${start.getMonth()}-${start.getFullYear()} ${start.getHours()}:${start.getMinutes()}`;
+    const endJson = `${end.getDay()}-${end.getMonth()}-${end.getFullYear()} ${end.getHours()}:${end.getMinutes()}`;
+    const signUp = data.get('SignUp');
+    const place = data.get('Place');
+    const image = data.get('image') as File;
+
+    const value = {
+      title: title,
+      description: description,
+      slug: slug,
+      registerUrl: signUp,
+      dateInterval: {
+        startDate: startJson,
+        endDate: endJson
+      },
+      location: place
+    };
+
+    const json = JSON.stringify(value);
+    const blob = new Blob([json], { type: 'application/json' });
+    const form = new FormData();
+    form.append('event', blob);
+    form.append('image', image, image.name);
+    console.log(form);
+    const success = await fetch(`/api/events`, {
+      method: 'POST',
+      body: form
+    }).then((res) => res.ok);
+
+    return success;
+  }
+};
---
+++ src/routes/(app)/events/edit/+page.svelte
@@ -0,0 +1,120 @@
+<script>
+  import PictureInput from '$lib/components/forms/picture-input.svelte';
+  import LabelInput from '$lib/components/forms/label-input.svelte';
+  import Button from '$lib/components/buttons/button.svelte';
+  import FormsHeader from '$lib/components/forms-header.svelte';
+
+  export let data;
+  let formData = { ...data.event };
+</script>
+
+
+<div class="flex h-full flex-col">
+  <FormsHeader label="Editar Evento" />
+
+  <div class="flex w-[100] flex-grow flex-col justify-around md:flex-row">
+    <form
+      method="POST"
+      enctype="multipart/form-data"
+      class="flex flex-col font-semibold md:flex-row md:gap-64"
+    >
+      <div class="order-2 ml-5 mt-5 flex flex-col content-start justify-center gap-10 md:order-1">
+        <LabelInput
+          label="Título"
+          name="Title"
+          type="text"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.Title}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Slug"
+          name="Slug"
+          type="text"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.Slug}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Início"
+          name="DateStart"
+          type="datetime-local"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.DateStart}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Fim"
+          name="DateEnd"
+          type="datetime-local"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.DateEnd}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Descrição"
+          name="Description"
+          type="text"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.Description}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Inscrição"
+          name="SignUp"
+          type="text"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.SignUp}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+        <LabelInput
+          label="Localização"
+          name="Place"
+          type="text"
+          placeholder="Insira o texto"
+          horizontal
+          textGap="25"
+          bind:value={formData.Place}
+          className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
+        />
+
+        <div class="ml-5 mr-5 flex flex-row gap-16 md:ml-0 md:flex-row">
+          <Button
+            type="button"
+            color="primary"
+            hoverColor="secondary"
+            width="large"
+            text="Selecionar Ficheiros"
+          />
+          <p class="flex items-center justify-center font-semibold">Nenhum Ficheiro Selecionado</p>
+        </div>
+
+        <div class="ml-5 mr-5 flex flex-row md:ml-0">
+          <Button
+            type="submit"
+            color="primary"
+            hoverColor="primary"
+            width="large"
+            text="Guardar Alterações"
+          />
+        </div>
+      </div>
+
+      <div class="order-1 mt-20 flex flex-col align-top md:order-2">
+        <PictureInput text="Insira a Imagem" name="image" />
+      </div>
+    </form>
+  </div>
+</div>
