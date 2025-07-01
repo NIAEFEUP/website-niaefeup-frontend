24872675cb054b36ffbed1844b78ced0f03cb337
Rui Soares <rui.rprs3@gmail.com>
2025-07-01 18:22:55 +0100

    connected to backend

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -1,7 +1,7 @@
 <script>
   import ErrorSection from './_components/error/error-section.svelte';
   import { page } from '$app/stores';
-  import Button from '$lib/components/button.svelte';
+  import Button from '$lib/components/buttons/button.svelte';
 </script>
 
 <main class="mx-10 my-20 flex flex-col justify-center gap-20 font-source_code">
--- src/routes/(app)/events/[slug]/_components/event-header.svelte
+++ src/routes/(app)/events/[slug]/_components/event-header.svelte
@@ -1,39 +1,38 @@
 <script lang="ts">
-  import { format } from 'date-fns';
-  import { pt } from 'date-fns/locale';
   import Icon from '@/lib/components/icons/icon.svelte';
   import Icons from '@/lib/components/icons/icons';
   import type { Event } from '@/types/event';
+  import moment from 'moment';
 
   import { Button } from '$lib/components/ui/button/index.js';
 
   export let data: Event;
-  let date = new Date(data.dateInterval.startDate);
-  let weekday = format(date, 'EEEE', { locale: pt });
-  let time = format(date, "HH'h'mm");
+  let event = data.event;
+  let momentDate = moment.utc(event.dateInterval.startDate, 'DD-MM-YYYY HH:mm').locale('pt');
+  let eventImage = "https://picsum.photos/500/500";
 </script>
 
 <div class="flex flex-col items-center gap-12">
   <div class="grid grid-cols-2 gap-12 md:grid-cols-3">
     <div class="space-y-12 text-white md:col-span-2">
-      <h1 class="hidden text-6xl font-bold md:block">{data.title}</h1>
+      <h1 class="hidden text-6xl font-bold md:block">{event.title}</h1>
       <ul class="space-y-4">
-        <li class="text-xl capitalize">{weekday} - {time}</li>
+        <li class="text-xl capitalize">{momentDate.format('dddd - HH[h]mm')}</li>
         <li class="flex items-center gap-2 text-xl font-bold">
           <Icon src={Icons.Calendar} color="white" size="20px" />
-          {format(date, 'd MMMM yyyy', { locale: pt })}
+          {momentDate.format('LL')}
         </li>
         <li class="flex w-full items-center gap-2 text-xl">
           <Icon src={Icons.Location} color="white" size="20px" />
-          {data.location}
+          {event.location}
         </li>
       </ul>
     </div>
     <div class="order-first flex flex-col items-center gap-4 md:order-last">
-      <img src={data.image} width="250" height="250" alt={data.title} />
+      <img class="rounded-lg" src={eventImage} width="250" height="250" alt={data.title} />
       <Button
         class="hidden border border-white bg-primary/50 px-12 py-6 font-bold uppercase md:flex"
-        href={data.registerUrl}
+        href={event.registerUrl}
       >
         Inscrever
       </Button>
@@ -41,7 +40,7 @@
   </div>
   <Button
     class="w-fit border border-white bg-primary/50 px-12 py-6 font-bold uppercase md:hidden"
-    href={data.registerUrl}
+    href={event.registerUrl}
   >
     Inscrever
   </Button>
