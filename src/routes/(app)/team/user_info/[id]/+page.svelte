8d845f73632d8289f3a8696ddc28d25fb6c3c0c2
Isaac3105 <isaac.absantos@gmail.com>
2025-07-06 17:17:36 +0100

    Started on the user page

---
+++ src/routes/(app)/team/user_info/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import FormsHeader from '$lib/components/forms-header.svelte';
+    import LabelInput from '$lib/components/forms/label-input.svelte';
+    import PictureInput from '$lib/components/forms/picture-input.svelte';
+    import Button from '$lib/components/buttons/button.svelte';
+</script>
+
+<div class = "flex flex-col gap-10">
+    <FormsHeader label="Informações Pessoais"/>
+
+    <div class = "flex flex-row justify-around">
+        
+        <div class = "flex flex-col">
+
+            <PictureInput text = "Foto de perfil" name="image" required = {true} />
+        
+            <Button/>
+        </div>
+
+    </div>
+
+</div>
