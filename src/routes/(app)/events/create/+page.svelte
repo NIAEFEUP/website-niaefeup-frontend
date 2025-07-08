a2404173eb0093ea2d65a59095d5c7d5cbf1bb7f
Isaac Santos <isaac.absantos@gmail.com>
2025-07-08 19:22:57 +0100

    Update src/routes/(app)/events/create/+page.svelte
    Co-authored-by: Rita Lopes <93883163+MRita443@users.noreply.github.com>

--- src/routes/(app)/events/create/+page.svelte
+++ src/routes/(app)/events/create/+page.svelte
@@ -62,7 +62,7 @@
           placeholder="Insira o texto"
           horizontal
           textGap=25
-          required="{true};"
+          required={true}
           className="flex-col ml-5 mr-5 md:flex-row md:ml-0 md:mr-0"
         />
         <LabelInput
