bcd06a0518cbb1fc2ad1d675426a016d270f3176
Isaac Santos <isaac.absantos@gmail.com>
2025-07-08 19:20:48 +0100

    Update src/lib/components/forms-header.svelte
    Co-authored-by: Rita Lopes <93883163+MRita443@users.noreply.github.com>

--- src/lib/components/forms-header.svelte
+++ src/lib/components/forms-header.svelte
@@ -2,8 +2,9 @@
   export let label = '';
 </script>
 
-<div class="flex w-[99vw] flex-row place-self-center border-b-[3px] border-b-muted-red-500 p-[2vw]">
-  <p class="ml-11 text-5xl font-bold">
+<div class="grow border-b-[3px] border-b-muted-red-500 pr-[2vw] pb-[2vw] pt-[1vw] mx-5">
+  <p class="ml-5 text-5xl font-bold">
     {label}
   </p>
+</div>
 </div>
