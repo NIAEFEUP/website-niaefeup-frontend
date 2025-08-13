312b47646adfc0483948ee0cc09122f0bc30c19e
Pedro Castro <falecompedroac@gmail.com>
2025-08-13 12:45:00 +0100

    sidebar links

--- src/routes/(app)/_components/layout/sidebar.svelte
+++ src/routes/(app)/_components/layout/sidebar.svelte
@@ -40,11 +40,11 @@
       {#each SidebarItems as item, i}
         <NavItem selected={selectedIndex === i}>
           <a
-            href="#/"
+            href={item.href}
             on:click={() => {
               selectedIndex = i;
               toggleSidebar();
-            }}>{item}</a
+            }}>{item.label}</a
           >
         </NavItem>
       {/each}
