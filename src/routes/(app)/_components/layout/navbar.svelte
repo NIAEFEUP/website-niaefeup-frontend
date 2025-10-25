7e082eada2114961089801adfeb5b97c0eb7dc09
DaltonDjoesman <daltonyuiti@hotmail.com>
2025-10-25 17:03:18 +0100

    storybook fixed

--- src/routes/(app)/_components/layout/navbar.svelte
+++ src/routes/(app)/_components/layout/navbar.svelte
@@ -1,9 +1,9 @@
 <script>
   import { onMount } from 'svelte';
   // @ts-expect-error Import is as expected but throws error
-  import { page } from '$app/state';
+  import { page } from '$app/stores';
 
-  let currentPage = $derived(page.url.pathname ?? '/');
+  let currentPage = $derived($page.url.pathname ?? '/');
   const links = [
     { href: '#', label: 'Equipa', pageComp: '/team' },
     { href: '/projects', label: 'Projetos', pageComp: '/projects' },
