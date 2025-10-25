2874842e3d7bff160585e0c71a3bceed13c8c0f4
DaltonDjoesman <daltonyuiti@hotmail.com>
2025-10-25 17:43:32 +0100

    lint fixed

--- src/routes/(app)/projects/_components/project-forms-input.stories.ts
+++ src/routes/(app)/projects/_components/project-forms-input.stories.ts
@@ -1,26 +1,24 @@
-import { Component } from 'lucide-svelte';
 import ProjectFormsInput from './project-forms-input.svelte';
-import type { Project } from '@/types/project';
 
-export default{
-    title: 'Pages/Project/ProjectFormsInput',
-    component: ProjectFormsInput,
-    parameters: {
-        layout: 'centered',
-    }
+export default {
+  title: 'Pages/Project/ProjectFormsInput',
+  component: ProjectFormsInput,
+  parameters: {
+    layout: 'centered'
+  }
 };
 
 export const notTextArea = {
-    args:{
-        label:"Title",
-        name:"title",
-    }
-}
+  args: {
+    label: 'Title',
+    name: 'title'
+  }
+};
 
 export const TextArea = {
-    args:{
-        label:"Title",
-        name:"title",
-        isTextArea:true,
-    }
-}
+  args: {
+    label: 'Title',
+    name: 'title',
+    isTextArea: true
+  }
+};
