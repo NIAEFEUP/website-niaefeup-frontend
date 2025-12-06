69abe5dca1ad38db5c28f6b88bf4cc385bf75bea
Pedro Lunet <pedrolunet@gmail.com>
2025-12-06 17:35:21 +0000

    added animated loading component

--- src/lib/components/layout/global-loader.svelte
+++ src/lib/components/layout/global-loader.svelte
@@ -1,9 +1,51 @@
 <script lang="ts">
-  // You can replace this HTML with a Lottie animation or specific SVG if you prefer
+  import NiLogo from '@/images/ni_negative_logo.svelte';
 </script>
 
 <div
-  class="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm transition-all duration-200"
+  class="fixed inset-0 z-[9999] flex items-center justify-center bg-black/60 backdrop-blur-sm transition-all duration-200"
 >
-  <div class="h-12 w-12 animate-spin rounded-full border-4 border-gray-600 border-t-white"></div>
+  <div class="custom-scaler">
+    <div class="custom-spinner">
+      <NiLogo class="block h-20 w-20" />
+    </div>
+  </div>
 </div>
+
+<style>
+  .custom-scaler {
+    will-change: transform;
+    animation: scale-up-down 4s infinite ease-in-out;
+  }
+
+  @keyframes scale-up-down {
+    0%,
+    100% {
+      transform: scale(0.8);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+  }
+
+  .custom-spinner {
+    will-change: transform;
+    animation: full-spin 2s infinite ease-in-out;
+  }
+
+  @keyframes full-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    .custom-scaler,
+    .custom-spinner {
+      animation: none;
+    }
+  }
+</style>
