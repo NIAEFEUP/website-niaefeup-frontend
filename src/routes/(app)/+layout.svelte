021b5e59f03932283bc98a92034d25e7c56696f2
Pedro Lunet <pedrolunet@gmail.com>
2025-12-07 12:42:40 +0000

    fixed linting

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import { navigating } from '$app/stores';
   import { ModeWatcher } from 'mode-watcher';
   import BackgroundHexagon from './_components/layout/background-hexagon.svelte';
   import Footer from './_components/layout/footer.svelte';
   import Navbar from './_components/layout/navbar.svelte';
   import Sidebar from './_components/layout/sidebar.svelte';
   import SnackbarList from './_components/layout/notifications/snackbar-list.svelte';
-  import GlobalLoader from '@/lib/components/layout/global-loader.svelte'; // Import the new component
-  import { isGlobalLoading } from '@/lib/stores/loader'; // Import the store
+  import GlobalLoader from '@/lib/components/layout/global-loader.svelte';
+  import { isGlobalLoading } from '@/lib/stores/loader';
+  import { navigating } from '$app/stores';
   import '@/app.css';
 
   interface Props {
