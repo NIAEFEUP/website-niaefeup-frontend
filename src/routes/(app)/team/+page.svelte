62b955aa25927822ac5c906dbe5eff27571568e5
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-12-17 08:49:53 +0000

    Added a team page

---
+++ src/routes/(app)/team/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="flex w-full flex-col items-center">
+  <h1 class="my-2 text-center font-source_code text-xl text-white md:my-4 md:text-3xl">
+    &lt&nbsp<strong>Equipa</strong>&nbsp/&gt
+  </h1>
+</div>
