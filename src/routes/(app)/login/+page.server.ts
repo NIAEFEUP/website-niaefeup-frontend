a35719cdc540597cae2a84b3eaf2337451cdd287
Hehehe-haw <dab.ferreira75@gmail.com>
2025-12-20 16:23:49 +0000

    lint test fixing

--- src/routes/(app)/login/+page.server.ts
+++ src/routes/(app)/login/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
-import {redirect} from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	let res = await fetch('/api/auth');
-    if (res.ok) {
-      throw redirect(302, '/');
-    }
+  let res = await fetch('/api/auth');
+  if (res.ok) {
+    throw redirect(302, '/');
+  }
 };
