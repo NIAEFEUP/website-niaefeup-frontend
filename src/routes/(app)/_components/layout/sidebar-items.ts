5e18474c3aa5f6fec3bea88edc0d0da6e927384f
carolinaroquee <carolina.roque.005@gmail.com>
2025-12-20 15:09:11 +0000

    fix/sideBar_mobile

--- src/routes/(app)/_components/layout/sidebar-items.ts
+++ src/routes/(app)/_components/layout/sidebar-items.ts
@@ -1,1 +1,7 @@
-export default Object.freeze<string[]>(['Início', 'Eventos', 'Projetos', 'Equipa', 'Contactos']);
+export default Object.freeze([
+  { label: 'Início', href: '/' },
+  { label: 'Eventos', href: '/events' },
+  { label: 'Projetos', href: '/projects' },
+  { label: 'Equipa', href: '/team' },
+  { label: 'Contactos', href: '/contacts' }
+]);
