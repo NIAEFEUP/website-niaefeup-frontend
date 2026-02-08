f6ea226f986c346d9c99adf75d670c1f63a35570
Iris <irigxu.2006@gmail.com>
2026-02-08 18:01:21 +0000

    fix: atualizando lógica no arquivo +page.ts

--- src/routes/(app)/user/[id]/+page.ts
+++ src/routes/(app)/user/[id]/+page.ts
@@ -16,11 +16,14 @@ export const load: PageLoad = async ({ fetch, params }) => {
     viewer = auth?.authenticated_user;
   }
 
-  if (viewer && String(viewer.id) === params.id) {
+  if (viewer && viewer.id === Number(params.id)) {
     canEdit = true;
-    // } else if (viewer) {
-    //   const permRes = await fetch('/api/auth/hasPermission/SUPERUSER');
-    //   if (permRes.ok) canEdit= true;
+  } else if (viewer) {
+    const permRes = await fetch('/api/auth/hasPermission/9'); // 9 is SUPERUSER
+    if (permRes.ok) {
+      const result = await permRes.json();
+      if (!result.error) canEdit = true;
+    }
   }
 
   return { teamMember, canEdit };
