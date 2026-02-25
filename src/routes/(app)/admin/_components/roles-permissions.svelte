cc004f50c3b614f10acafeaae4b1d2759cd0517b
Pedro Lunet <pedrolunet@gmail.com>
2026-02-25 16:24:07 +0000

    added roles premissions component

---
+++ src/routes/(app)/admin/_components/roles-permissions.svelte
@@ -0,0 +1,6 @@
+<div class="flex h-full w-[500px] flex-col gap-y-4 p-8">
+  <div class="mb-12 flex items-center justify-between">
+      <h2 class="text-3xl font-bold text-white">Atividade</h2>
+    </div>
+    oi
+</div>
