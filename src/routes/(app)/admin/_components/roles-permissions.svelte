e9f6d74fadccb8df3d3bf1d7f49d7828c5980bb5
Pedro Lunet <pedrolunet@gmail.com>
2026-03-02 09:04:57 +0000

    used dropdown component

--- src/routes/(app)/admin/_components/roles-permissions.svelte
+++ src/routes/(app)/admin/_components/roles-permissions.svelte
@@ -1,6 +1,14 @@
+<script>
+  import Dropdown from './dropdown.svelte';
+</script>
+
 <div class="flex h-full w-[500px] flex-col gap-y-4 p-8">
   <div class="mb-12 flex items-center justify-between">
     <h2 class="text-3xl font-bold text-white">Atividade</h2>
+    <Dropdown
+      options={['Opção 1', 'Opção 2', 'Opção 3']}
+      on:select={(e) => console.log('Selected:', e.detail)}
+    />
   </div>
   oi
 </div>
