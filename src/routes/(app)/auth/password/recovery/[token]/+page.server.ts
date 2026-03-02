e289595d2823dbad848c3d9d5cac8bb1f09fdd7b
Hehehe-haw <dab.ferreira75@gmail.com>
2026-03-02 21:41:26 +0000

    Started implementing the password recovery page

---
+++ src/routes/(app)/auth/password/recovery/[token]/+page.server.ts
@@ -0,0 +1,34 @@
+import type { PageServerLoad, Actions } from './$types';
+
+
+export const load: PageServerLoad = async ({params}) => {
+    return {
+        token: params.token
+    };
+    
+};
+
+export const actions: Actions = {
+    submitRecovery: async ({ request, fetch }) => {
+
+        const requestData = await request.formData();
+        const password = requestData.get('password');
+        const confirmpassword = requestData.get('confirmpassword');
+        const token = requestData.get('token');
+        let stringpassword = $state('')
+        if (password != null){
+            stringpassword = password.toString()
+        }
+        
+
+        let check = password != '' && password == confirmpassword;
+        if (check){
+            const formData = new FormData();
+            formData.append('password', stringpassword);
+            const res = await fetch(`api/auth/password/recovery/${token}/confirm`, {
+                method: 'POST',
+                body: formData
+            });
+        };
+    }
+};
---
+++ src/routes/(app)/auth/password/recovery/[token]/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import type {PageData} from './$types';
+    import { enhance } from '$app/forms';
+    let {data}: {data:PageData} = $props();
+
+    const token: string = data.token;
+
+    let password = $state('');
+    let confirmpassword = $state('');
+
+    
+</script>
+
+<p>Recovery token: {token}</p>
+
+<section>
+    
+    <form
+        method="POST"
+        action="?/submitRecovery"
+        
+    >
+        <div>
+            <h2>New Password</h2>
+            <input
+                type = "text"
+                placeholder="New Password"
+                bind:value={password}
+                id="password"
+            >
+        </div>
+        <div>
+            <h2>Confirm Password</h2>
+            <input
+                type = "text"
+                placeholder = "Confirm Password"
+                bind:value={confirmpassword}
+                id="confirmpassword"
+            >
+        </div>
+        <button type='submit'>submit</button>
+        <!-- <button onclick={test}>sub</button> -->
+    </form>
+
+    
+</section>
