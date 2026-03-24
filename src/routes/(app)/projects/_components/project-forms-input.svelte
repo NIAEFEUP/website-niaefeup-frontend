010479a277d992f1a5a731d2f425386a75f21af4
DaltonDjoesman <daltonyuiti@hotmail.com>
2026-03-24 09:02:52 +0000

    added some properties in the project-forms-input component

--- src/routes/(app)/projects/_components/project-forms-input.svelte
+++ src/routes/(app)/projects/_components/project-forms-input.svelte
@@ -7,23 +7,35 @@
     horizontal: boolean;
     isTextArea?: boolean;
     value?: string;
+    type?: string;
+    required?: boolean;
+    minlength?: number;
+    maxlength?: number;
   }
   let {
     label = '',
     name = '',
     horizontal = false,
     isTextArea = false,
-    value = $bindable('')
+    value = $bindable(''),
+    type = 'text',
+    required = false,
+    minlength = undefined,
+    maxlength = undefined
   }: Props = $props();
 </script>
 
 <LabelInput
   {label}
   {name}
+  {type}
   {horizontal}
   {isTextArea}
   placeholder="Insira o texto"
   className="flex-col md:flex-row"
   textGap="10"
+  {required}
+  {minlength}
+  {maxlength}
   bind:value
 />
