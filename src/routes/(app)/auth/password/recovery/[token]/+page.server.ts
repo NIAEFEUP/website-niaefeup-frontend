a07abd3056b7eafe44c7ccf194ca0e9ed4305301
David Ferreira <dab.ferreira75@gmail.com>
2026-03-25 20:24:47 +0000

    Small changes

--- src/routes/(app)/auth/password/recovery/[token]/+page.server.ts
+++ src/routes/(app)/auth/password/recovery/[token]/+page.server.ts
@@ -1,44 +1,43 @@
 import type { PageServerLoad, Actions } from './$types';
 
 function getResult(pw: string, pw_confirm: string): string {
-    if (pw !== pw_confirm)
-        return "Passwords don't match!";
-    return "";
+  if (pw !== pw_confirm) return "Passwords don't match!";
+  return '';
 }
 
 export const load: PageServerLoad = async ({ params }) => {
-    return {
-        token: params.token
-    };
+  return {
+    token: params.token
+  };
 };
 
 export const actions: Actions = {
-    submitRecovery: async ({ request, fetch }) => {
-        const requestData = await request.formData();
-        const password = requestData.get('password');
-        const confirmpassword = requestData.get('confirmpassword');
-        const token = requestData.get('token');
+  submitRecovery: async ({ request, fetch }) => {
+    const requestData = await request.formData();
+    const password = requestData.get('password');
+    const confirmpassword = requestData.get('confirmpassword');
+    const token = requestData.get('token');
 
-        let stringpassword = password != null ? password.toString() : '';
-        let stringpassword_confirm = confirmpassword != null ? confirmpassword.toString() : '';
+    let stringpassword = password != null ? password.toString() : '';
+    let stringpassword_confirm = confirmpassword != null ? confirmpassword.toString() : '';
 
-        const err = getResult(stringpassword, stringpassword_confirm);
-        if (err) return { success: false, err };
-        
-        const res = await fetch(`/api/auth/password/recovery/${token}/confirm`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ password: stringpassword })
-        });
+    const err = getResult(stringpassword, stringpassword_confirm);
+    if (err) return { success: false, err };
 
-        if (!res.ok) {
-            const data = await res.json();
-            const message = data.errors[0].message;
-            return { success: false, err: message };
-        }
+    const res = await fetch(`/api/auth/password/recovery/${token}/confirm`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ password: stringpassword })
+    });
 
-        console.log(await res.text());
-
-        return { success: true, err: '' };
+    if (!res.ok) {
+      const data = await res.json();
+      const message = data.errors[0].message;
+      return { success: false, err: message };
     }
+
+    console.log(await res.text());
+
+    return { success: true, err: '' };
+  }
 };
--- src/routes/(app)/auth/password/recovery/[token]/+page.svelte
+++ src/routes/(app)/auth/password/recovery/[token]/+page.svelte
@@ -1,50 +1,49 @@
 <script lang="ts">
-    import type { PageData, ActionData } from './$types';
-    import { enhance } from '$app/forms';
-    import { goto } from '$app/navigation';
-    let { data, form }: { data: PageData; form: ActionData } = $props();
+  import type { PageData, ActionData } from './$types';
+  import { enhance } from '$app/forms';
+  import { goto } from '$app/navigation';
+  let { data, form }: { data: PageData; form: ActionData } = $props();
 
-    const token: string = data.token;
+  const token: string = data.token;
 
-    let password = $state('');
-    let confirmpassword = $state('');
+  let password = $state('');
+  let confirmpassword = $state('');
 
-    $effect(() => {
-        if (form?.success) {
-            goto('/login');
-        }
-    });
-
+  $effect(() => {
+    if (form?.success) {
+      goto('/login');
+    }
+  });
 </script>
 
 <section>
-    <form method="POST" action="?/submitRecovery" use:enhance>
-        <div>
-            <h2>New Password</h2>
-            <input
-                type="password"
-                name="password"
-                placeholder="New Password"
-                bind:value={password}
-                id="password"
-            />
-        </div>
-        <div>
-            <h2>Confirm Password</h2>
-            <input
-                type="password"
-                name="confirmpassword"
-                placeholder="Confirm Password"
-                bind:value={confirmpassword}
-                id="confirmpassword"
-            />
-        </div>
-        <input type="hidden" name="token" value={token} />
+  <form method="POST" action="?/submitRecovery" use:enhance>
+    <div>
+      <h2>New Password</h2>
+      <input
+        type="password"
+        name="password"
+        placeholder="New Password"
+        bind:value={password}
+        id="password"
+      />
+    </div>
+    <div>
+      <h2>Confirm Password</h2>
+      <input
+        type="password"
+        name="confirmpassword"
+        placeholder="Confirm Password"
+        bind:value={confirmpassword}
+        id="confirmpassword"
+      />
+    </div>
+    <input type="hidden" name="token" value={token} />
 
-        {#if form?.err}
-            <p>{form.err}</p>
-        {/if}
+    {#if form?.err}
+      <p>{form.err}</p>
+    {/if}
 
-        <button type="submit">Submit</button>
-    </form>
+    <button type="submit">Submit</button>
+  </form>
 </section>
