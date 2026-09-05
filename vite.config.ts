fb5cbc588aabb59f95ff4e04fa171960c391de21
Pedro Castro <falecompedroac@gmail.com>
2026-09-05 17:31:13 +0100

    align config with sv create standards

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit({
+      alias: {
+        '@': './src',
+        '@/*': './src/*'
+      },
+      adapter: adapter(),
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+      }
+    })
+  ],
   test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    passWithNoTests: true
+    expect: { requireAssertions: true },
+    passWithNoTests: true,
+    projects: [
+      {
+        extends: './vite.config.ts',
+        test: {
+          name: 'server',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+        }
+      }
+    ]
   }
 });
