458f149ed322cd01191b393ff250956b3d42ce41
Pedro Castro <falecompedroac@gmail.com>
2026-09-05 15:08:11 +0100

    remove dead backend proxy and auth code

--- src/lib/api/proxy.ts
+++
@@ -1,4 +0,0 @@
-export function endpoint(url: URL) {
-  const pathName = url.pathname.replace('/api', '').replace('/__list', '');
-  return pathName.startsWith('/') ? pathName.substring(1) : pathName;
-}
--- src/routes/(app)/_components/layout/footer.svelte
+++ src/routes/(app)/_components/layout/footer.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  //eslint-disable-next-line @typescript-eslint/no-unused-vars
-  import MemberButton from './member-button.svelte';
   import { createNotification } from './notifications';
   import NotificationMessages from './notifications/notification-messages';
   import Icon from '$lib/components/icons/icon.svelte';
@@ -103,8 +101,6 @@
       <p class="text-sm">NIAEFEUP &copy; 2022</p>
     </div>
     <div class="flex flex-row items-center justify-end gap-4 self-center p-3">
-      <!-- Disabled while the site is fully static / backend is offline -->
-      <!-- <MemberButton /> -->
       <div class="text-right text-sm">
         <p class="font-bold">FEUP</p>
         <p>Sala B315</p>
@@ -178,10 +174,6 @@
       >
         <Icon src={Icons.Mail} color="white" size="24px" />
       </div>
-    </div>
-    <div class="flex flex-row items-center justify-end gap-4 self-center p-3">
-      <!-- Disabled while the site is fully static / backend is offline -->
-      <!-- <MemberButton /> -->
     </div>
   </footer>
 </div>
