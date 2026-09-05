c41847a30ca15741f9c19ae9aaed73027dba28db
Pedro Castro <falecompedroac@gmail.com>
2026-09-05 17:14:33 +0100

    clean up project type imports

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,11 +1,12 @@
 import type { Technology } from './technology';
 import type { CustomWebsite } from './custom-website';
 import type { TimelineEvent } from './timeline-event';
+import type { TeamMember } from './team-member';
 
 export type Project = {
   title: string;
   description: string;
-  teamMembers: import('./team-member').TeamMember[];
+  teamMembers: TeamMember[];
   slug: string;
   image: string;
   gallery: string[];
@@ -16,6 +17,6 @@ export type Project = {
   targetAudience: string;
   github: string;
   links: CustomWebsite[];
-  hallOfFame: import('./team-member').TeamMember[];
+  hallOfFame: TeamMember[];
   timeline: TimelineEvent[];
 };
